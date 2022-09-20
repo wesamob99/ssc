1b5c790499365a5ce93d239939cdf05584778f41
wesam bakar <wesam.bakar@mobisoft-me.com>
2022-09-20 14:41:00 +0300

    new files HTTPClientContract.dart & UserSecuredStorage.dart

---
+++ lib/infrastructure/HTTPClientContract.dart
@@ -0,0 +1,175 @@
+// ignore_for_file: constant_identifier_names, prefer_typing_uninitialized_variables
+
+import 'dart:convert';
+import 'package:dio/dio.dart';
+import 'package:ssc/infrastructure/userConfig.dart';
+import '../utilities/util.dart';
+import 'UserSecuredStorage.dart';
+
+//Local testing http://192.168.2.44:9000/
+//Staging  https://realsoftapps.com/Adaa_Stage/adaastageapi/
+//Normal   https://realsoftapps.com/Adaa_new/adaaapi
+
+dynamic httpErrorMessage(dynamic responseData) {
+  var jsonData;
+  if (responseData != null && !isHTML(responseData.data)) {
+    jsonData = jsonDecode(responseData.data);
+  }
+  if (jsonData != null && jsonData['customCode'] != null) {
+    return UserConfig.instance.checkLanguage()
+        ? jsonData['translatable']['en']
+        : jsonData['translatable']['ar'];
+  } else {
+    return UserConfig.instance.checkLanguage()
+        ? 'Server Error'
+        : 'خطا من الخادم';
+  }
+}
+
+///Current URL :Staging
+class HTTPClientContract {
+  //static const String BASE_URL = 'http://192.168.2.44:9000/';
+  static const String BASE_URL = 'http://192.168.2.44:9000/';
+  static final _dio = _createDio();
+  static const int _MAX_RETRY = 7;
+  static const ResponseType _DEFAULT_RESPONSE_TYPE = ResponseType.plain;
+  static int _retryCount = 0;
+  static CancelToken? _cancelToken;
+  static final _baseAPI = _addInterceptors(_dio);
+  static final bytesAPI = _addInterceptors(_createBytesDio());
+  static HTTPClientContract? _instance;
+
+  HTTPClientContract._();
+
+  static HTTPClientContract get instance =>
+      _instance ??= HTTPClientContract._();
+
+  static final BaseOptions _opts = BaseOptions(
+    baseUrl: BASE_URL,
+    responseType: _DEFAULT_RESPONSE_TYPE,
+    connectTimeout: 30000,
+    receiveTimeout: 60000,
+    receiveDataWhenStatusError: true,
+    validateStatus: (status) => true,
+  );
+
+  static Dio _createDio() {
+    _cancelToken = CancelToken();
+    return Dio(_opts);
+  }
+
+  static Dio _createBytesDio() {
+    _cancelToken = CancelToken();
+    return Dio(BaseOptions(
+      baseUrl: BASE_URL,
+      responseType: ResponseType.bytes,
+      connectTimeout: 30000,
+      receiveTimeout: 60000,
+      receiveDataWhenStatusError: true,
+      validateStatus: (status) => true,
+    ));
+  }
+
+  static Dio _addInterceptors(Dio dio) {
+    return dio
+      ..interceptors.add(InterceptorsWrapper(onRequest: (options, handler) {
+        options.headers.addAll({
+          if (UserSecuredStorage.instance.token.isNotEmpty)
+            "Authorization": UserSecuredStorage.instance.token,
+          // Headers.contentTypeHeader: Headers.jsonContentType,
+        });
+        return handler.next(options);
+      }, onError: (e, handler) async {
+        if ((e.response?.statusCode == 403 || e.response?.statusCode == 401)) {
+          try {
+            if (_retryCount > _MAX_RETRY) {
+              _retryCount = 0;
+              return handler.next(e);
+            }
+            _retryCount++;
+            final RequestOptions? options = e.response?.requestOptions;
+
+            // await UserSecuredStorage.instance
+            //     .refreshToken(UserSecuredStorage.instance.token);
+            options?.headers['Authorization'] =
+                UserSecuredStorage.instance.token;
+            final Response response = await dio.fetch(options!);
+            _retryCount = 0;
+            return handler.resolve(response);
+          } on DioError catch(e){
+            // if (e.response.realUri.toString().contains('RefreshExpiredToken'))
+            //   await _sessionTimeout();
+            // else
+            _retryCount = 0;
+            return handler.next(e);
+          }
+        } else {
+          _retryCount = 0;
+          // if (e != null &&
+          //     e.response != null &&
+          //     e.response.realUri.toString().contains('RefreshExpiredToken'))
+          //   await _sessionTimeout();
+          // else
+          return handler.next(e);
+        }
+      }, onResponse: (response, handler) {
+        return handler.next(response);
+      }));
+  }
+
+  Future<Response?> getHTTP(String url) async {
+    try {
+      Response response = await _baseAPI.get(url, cancelToken: _cancelToken);
+      return response;
+    } on DioError catch (e) {
+      if (CancelToken.isCancel(e)) {
+        // showFlushBar(
+        //     UserConfig.instance.checkLanguage() ? 'Download' : 'التحميل',
+        //     UserConfig.instance.checkLanguage()
+        //         ? 'File downloading canceled by user'
+        //         : 'تم إلغاء تنزيل الملف من قبل المستخدم',
+        //     navigatorKey.currentState.context);
+      }
+      return e.response;
+    }
+  }
+
+  Future<Response?> postHTTP(String url, dynamic data) async {
+    try {
+      Response response = await _baseAPI.post(url, data: data);
+
+      return response;
+    } on DioError catch (e) {
+      return e.response;
+    }
+  }
+
+  Future<Response?> putHTTP(String url, dynamic data) async {
+    try {
+      Response response = await _baseAPI.put(url, data: data);
+      return response;
+    } on DioError catch (e) {
+      return e.response;
+    }
+  }
+
+  Future<Response?> deleteHTTP(String url) async {
+    try {
+      Response response = await _baseAPI.delete(url);
+      return response;
+    } on DioError catch (e) {
+      return e.response;
+    }
+  }
+
+  Future<Response?> patchHTTP(String url, dynamic data) async {
+    try {
+      Response response =
+          await _baseAPI.patch(url, data: data);
+      return response;
+    } on DioError catch (e) {
+      return e.response;
+    }
+  }
+
+}
---
+++ lib/infrastructure/UserSecuredStorage.dart
@@ -0,0 +1,158 @@
+// ignore_for_file: constant_identifier_names, prefer_typing_uninitialized_variables
+
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:ssc/infrastructure/HTTPClientContract.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart' as hive_flutter;
+
+class UserSecuredStorage {
+  static const String _BOX_NAME = 'user_key';
+  String _email = "";
+  String _userNameEn = "";
+  String _userNameAr = "";
+  String _empID = "";
+  String _userImage = "";
+  String _userJson = "";
+  String _token = "";
+  bool _isAdmin = false;
+  var _box;
+
+  static UserSecuredStorage? _instance;
+
+  UserSecuredStorage._();
+
+  static UserSecuredStorage get instance =>
+      _instance ??= UserSecuredStorage._();
+
+  String get userJson {
+    _userJson = _box.get('user');
+    return _userJson;
+  }
+
+  bool get isAdmin {
+    _isAdmin = _box.get('isAdmin');
+    return _isAdmin;
+  }
+
+  set isAdmin(bool value) {
+    addKeyPair('isAdmin', value);
+
+    _isAdmin = value;
+  }
+
+  set userJson(String value) {
+    addKeyPair('user', value);
+    _userJson = value;
+  }
+
+  String get empID {
+    _empID = _box.get('empID');
+    return _empID;
+  }
+
+  set empID(String value) {
+    addKeyPair('empID', value);
+    _empID = value;
+  }
+
+  String get token {
+    _token = _box.get('token');
+    return _token;
+  }
+
+  set token(String value) {
+    addKeyPair('token', value);
+
+    _token = value;
+  }
+
+  String get email {
+    _email = _box.get('email');
+    return _email;
+  }
+
+  set email(String value) {
+    addKeyPair('email', value);
+    _email = value;
+  }
+
+  String get userNameAr {
+    _userNameEn = _box.get('userNameAr');
+    return _userNameAr;
+  }
+
+  set userNameAr(String value) {
+    addKeyPair('userNameAr', value);
+    _userNameAr = value;
+  }
+
+  String get userNameEn {
+    _userNameEn = _box.get('userName');
+    return _userNameEn;
+  }
+
+  set userNameEn(String value) {
+    addKeyPair('userName', value);
+    _userNameEn = value;
+  }
+
+  String get userImage {
+    _userImage = _box.get('userImage');
+    return _userImage;
+  }
+
+  set userImage(String value) {
+    addKeyPair('userImage', value);
+    _userImage = value;
+  }
+
+  void addKeyPair(dynamic key, dynamic value) {
+    _box.put(key, value);
+  }
+//
+  Future<void> initSecuredBox() async {
+    await hive_flutter.Hive.initFlutter();
+    if (Platform.isAndroid) {
+      const FlutterSecureStorage secureStorage = FlutterSecureStorage();
+      var containsEncryptionKey =
+          await secureStorage.containsKey(key: _BOX_NAME);
+      if (!containsEncryptionKey) {
+        await secureStorage.write(
+            key: _BOX_NAME, value: base64UrlEncode(Hive.generateSecureKey()));
+      }
+
+      String? read = await secureStorage.read(key: _BOX_NAME);
+      await UserSecuredStorage.instance.openBox(
+          hiveCipher: HiveAesCipher(base64Url.decode(read!)));
+    } else {
+      await UserSecuredStorage.instance.openBox();
+    }
+  }
+
+  Future<void> openBox({HiveCipher? hiveCipher}) async {
+    _box = await Hive.openBox(_BOX_NAME,
+        encryptionCipher: Platform.isAndroid ? hiveCipher : null);
+  }
+
+  Future<void> clearUserData() async {
+    await _box.clear();
+  }
+
+  Future refreshToken(String oldToken) async {
+    var response = await HTTPClientContract.instance.postHTTP(
+        'RefreshExpiredToken',
+        jsonEncode({
+          "TokenToRefresh": oldToken,
+        }));
+
+    if (response != null && response.statusCode == 200) {
+      var myMap = jsonDecode(response.data);
+      _box.put('token', myMap['Token'].toString());
+      token = myMap['Token'].toString();
+      return 'ok';
+    }
+  }
+}
