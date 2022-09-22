ee18dfb929dd0dd8b717358978a1b465093962d7
wesamobeidat <wesam.obe@hotmail.com>
2022-09-22 21:59:35 +0300

    add slideShow, bottomNavigationBar

--- lib/src/view/home/homeScreen.dart
+++ lib/src/view/home/homeScreen.dart
@@ -1,4 +1,8 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_image_slideshow/flutter_image_slideshow.dart';
+import 'package:ssc/utilities/hexColor.dart';
 
 import '../../../utilities/util.dart';
 
@@ -17,7 +21,69 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text(translate('home', context)),
         centerTitle: true,
       ),
-      body: Container(),
+      body: Column(
+        children: [
+          imageSlideShow(),
+        ],
+      ),
+      bottomNavigationBar: curvedNavigationBar(),
     );
   }
+
+  imageSlideShow(){
+    return ImageSlideshow(
+        width: double.infinity,
+        height: height(.23, context),
+        initialPage: 0,
+        indicatorColor: Colors.blue,
+        indicatorBackgroundColor: Colors.grey,
+        onPageChanged: (value) {
+          // print('Page changed: $value');
+        },
+        autoPlayInterval: 3000,
+        isLoop: true,
+        children: [
+          InkWell(
+            onTap: (){},
+            child: Image.asset(
+              'assets/images/test_slider_images/1.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          InkWell(
+            onTap: (){},
+            child: Image.asset(
+              'assets/images/test_slider_images/2.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          InkWell(
+            onTap: (){},
+            child: Image.asset(
+              'assets/images/test_slider_images/3.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ]
+    );
+  }
+
+  curvedNavigationBar(){
+    return CurvedNavigationBar(
+      backgroundColor: Colors.transparent,
+      color: HexColor('#445740'),
+      buttonBackgroundColor: HexColor('#445740'),
+      items: const <Widget>[
+        Icon(Icons.home, size: 30, color: Colors.white),
+        Icon(Icons.list, size: 30, color: Colors.white),
+        Icon(Icons.compare_arrows, size: 30, color: Colors.white),
+        Icon(Icons.add_alert, size: 30, color: Colors.white),
+        Icon(Icons.settings, size: 30, color: Colors.white),
+      ],
+      onTap: (index) {
+        //Handle button tap
+      },
+    );
+  }
+
 }
