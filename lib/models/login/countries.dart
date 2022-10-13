8ad5da70d22153f50f848914a157921f7b6e3a3e
wesamobeidat <wesam.obe@hotmail.com>
2022-10-13 15:17:54 +0300

    update countries.dart model

--- lib/models/login/countries.dart
+++ lib/models/login/countries.dart
@@ -28,22 +28,22 @@ class Countries {
   final int natcode;
 
   factory Countries.fromJson(Map<String, dynamic> json) => Countries(
-    country: json["country"] == null ? null : json["country"],
-    countryEn: json["country_en"] == null ? null : json["country_en"],
-    callingCode: json["calling_code"] == null ? null : json["calling_code"],
-    paymethodType: json["PAYMETHOD_TYPE"] == null ? null : json["PAYMETHOD_TYPE"],
-    ibanLength: json["IBAN_LENGTH"] == null ? null : json["IBAN_LENGTH"],
-    ibanStartWith: json["IBAN_START_WITH"] == null ? null : json["IBAN_START_WITH"],
-    natcode: json["NATCODE"] == null ? null : json["NATCODE"],
+    country: json["country"],
+    countryEn: json["country_en"],
+    callingCode: json["calling_code"],
+    paymethodType: json["PAYMETHOD_TYPE"],
+    ibanLength: json["IBAN_LENGTH"],
+    ibanStartWith: json["IBAN_START_WITH"],
+    natcode: json["NATCODE"],
   );
 
   Map<String, dynamic> toJson() => {
-    "country": country == null ? null : country,
-    "country_en": countryEn == null ? null : countryEn,
-    "calling_code": callingCode == null ? null : callingCode,
-    "PAYMETHOD_TYPE": paymethodType == null ? null : paymethodType,
-    "IBAN_LENGTH": ibanLength == null ? null : ibanLength,
-    "IBAN_START_WITH": ibanStartWith == null ? null : ibanStartWith,
-    "NATCODE": natcode == null ? null : natcode,
+    "country": country,
+    "country_en": countryEn,
+    "calling_code": callingCode,
+    "PAYMETHOD_TYPE": paymethodType,
+    "IBAN_LENGTH": ibanLength,
+    "IBAN_START_WITH": ibanStartWith,
+    "NATCODE": natcode,
   };
 }
