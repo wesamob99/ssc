e8152ccb4ddcd462cc5f4a373003ae18695b6fec
wesamobeidat <wesam.obe@hotmail.com>
2022-10-16 10:22:42 +0300

    create registerData.dart model

---
+++ lib/models/login/registerData.dart
@@ -0,0 +1,137 @@
+// To parse this JSON data, do
+//
+//     final registerData = registerDataFromJson(jsonString);
+
+import 'dart:convert';
+
+RegisterData registerDataFromJson(String str) => RegisterData.fromJson(json.decode(str));
+
+String registerDataToJson(RegisterData data) => json.encode(data.toJson());
+
+class RegisterData {
+  RegisterData({
+    this.nationality,
+    this.nationalNumber,
+    this.personalNumber,
+    this.personalCardNo,
+    this.relativeNatId,
+    this.relativeType,
+    this.gender,
+    this.dateOfBirth,
+    this.countryCodeNo,
+    this.mobileNo,
+    this.passportNo,
+    this.dateOfEntry,
+    this.insuranceNo,
+    this.residentCountry,
+    this.academicLevel,
+    this.email,
+    this.confirmEmail,
+    this.bankCode,
+    this.iban,
+    this.password,
+    this.userId,
+    this.activationBy,
+    this.language,
+    this.recaptchaReactive,
+    this.nationalId,
+    this.nationalMobile,
+    this.nationalMobileCode,
+    this.hashedCaptcha,
+    this.isWebsite,
+  });
+
+  final int nationality;
+  final int nationalNumber;
+  final dynamic personalNumber;
+  final String personalCardNo;
+  final int relativeNatId;
+  final int relativeType;
+  final dynamic gender;
+  final dynamic dateOfBirth;
+  final String countryCodeNo;
+  final String mobileNo;
+  final dynamic passportNo;
+  final dynamic dateOfEntry;
+  final dynamic insuranceNo;
+  final int residentCountry;
+  final String academicLevel;
+  final String email;
+  final String confirmEmail;
+  final dynamic bankCode;
+  final dynamic iban;
+  final String password;
+  final int userId;
+  final int activationBy;
+  final int language;
+  final String recaptchaReactive;
+  final dynamic nationalId;
+  final dynamic nationalMobile;
+  final dynamic nationalMobileCode;
+  final String hashedCaptcha;
+  final bool isWebsite;
+
+  factory RegisterData.fromJson(Map<String, dynamic> json) => RegisterData(
+    nationality: json["nationality"],
+    nationalNumber: json["nationalNumber"],
+    personalNumber: json["personalNumber"],
+    personalCardNo: json["personalCardNo"],
+    relativeNatId: json["relativeNatId"],
+    relativeType: json["relativeType"],
+    gender: json["gender"],
+    dateOfBirth: json["dateOfBirth"],
+    countryCodeNo: json["countryCodeNo"],
+    mobileNo: json["mobileNo"],
+    passportNo: json["passportNo"],
+    dateOfEntry: json["dateOfEntry"],
+    insuranceNo: json["insuranceNo"],
+    residentCountry: json["residentCountry"],
+    academicLevel: json["academicLevel"],
+    email: json["email"],
+    confirmEmail: json["confirmEmail"],
+    bankCode: json["bankCode"],
+    iban: json["iban"],
+    password: json["password"],
+    userId: json["userId"],
+    activationBy: json["activationBy"],
+    language: json["language"],
+    recaptchaReactive: json["recaptchaReactive"],
+    nationalId: json["nationalID"],
+    nationalMobile: json["nationalMobile"],
+    nationalMobileCode: json["nationalMobileCode"],
+    hashedCaptcha: json["hashedCaptcha"],
+    isWebsite: json["isWebsite"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "nationality": nationality,
+    "nationalNumber": nationalNumber,
+    "personalNumber": personalNumber,
+    "personalCardNo": personalCardNo,
+    "relativeNatId": relativeNatId,
+    "relativeType": relativeType,
+    "gender": gender,
+    "dateOfBirth": dateOfBirth,
+    "countryCodeNo": countryCodeNo,
+    "mobileNo": mobileNo,
+    "passportNo": passportNo,
+    "dateOfEntry": dateOfEntry,
+    "insuranceNo": insuranceNo,
+    "residentCountry": residentCountry,
+    "academicLevel": academicLevel,
+    "email": email,
+    "confirmEmail": confirmEmail,
+    "bankCode": bankCode,
+    "iban": iban,
+    "password": password,
+    "userId": userId,
+    "activationBy": activationBy,
+    "language": language,
+    "recaptchaReactive": recaptchaReactive,
+    "nationalID": nationalId,
+    "nationalMobile": nationalMobile,
+    "nationalMobileCode": nationalMobileCode,
+    "hashedCaptcha": hashedCaptcha,
+    "isWebsite": isWebsite,
+  };
+}
