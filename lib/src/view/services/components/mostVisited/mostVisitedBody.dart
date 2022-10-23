2b71cace11d745e63e3136a357a089bd6c61833f
wesamobeidat <wesam.obe@hotmail.com>
2022-10-23 15:19:38 +0300

    update mostVisitedBody.dart

--- lib/src/view/services/components/mostVisited/mostVisitedBody.dart
+++ lib/src/view/services/components/mostVisited/mostVisitedBody.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 
 import '../../../../../utilities/hexColor.dart';
 import '../../../../../utilities/util.dart';
+import '../../complaints/workInjuryComplaintScreen.dart';
 
 class MostVisitedBody extends StatefulWidget {
   const MostVisitedBody({Key key}) : super(key: key);
@@ -31,25 +32,32 @@ class _MostVisitedBodyState extends State<MostVisitedBody> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Container(
-                  margin: const EdgeInsets.only(right: 10),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        translate(mostVisitedServices[index]['title'], context),
-                        style: TextStyle(
-                          fontSize: width(0.035, context)
+                InkWell(
+                  onTap: (){
+                    Navigator.of(context).push(
+                      MaterialPageRoute(builder: (context) => const WorkInjuryComplaintScreen())
+                    );
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          translate(mostVisitedServices[index]['title'], context),
+                          style: TextStyle(
+                            fontSize: width(0.035, context)
+                          ),
                         ),
-                      ),
-                      SizedBox(height: height(0.006, context)),
-                      Text(
-                        translate(mostVisitedServices[index]['subTitle'], context),
-                        style: TextStyle(
-                          fontSize: width(0.03, context)
+                        SizedBox(height: height(0.006, context)),
+                        Text(
+                          translate(mostVisitedServices[index]['subTitle'], context),
+                          style: TextStyle(
+                            fontSize: width(0.03, context)
+                          ),
                         ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
                 ),
                 Divider(
