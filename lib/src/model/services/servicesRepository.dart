1fcc48c1163202f2240a454ecc77867b665fcc6d
wesamobeidat <wesam.obe@hotmail.com>
2022-10-24 12:14:28 +0300

    create servicesRepository.dart

---
+++ lib/src/model/services/servicesRepository.dart
@@ -0,0 +1,23 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/foundation.dart';
+import 'package:ssc/infrastructure/userSecuredStorage.dart';
+
+import '../../../infrastructure/HTTPClientContract.dart';
+import '../../../models/profile/userProfileData.dart';
+
+class ServicesRepository{
+
+  Future<UserProfileData> getAccountDataService() async {
+    UserSecuredStorage userSecuredStorage = UserSecuredStorage.instance;
+    String internalKey = userSecuredStorage.internalKey.toString();
+    var response = await HTTPClientContract.instance.getHTTP('/individuals/GET_INDIVIDUALUSERINFOSP?PIINSURANCENO=$internalKey');
+    if (kDebugMode) {
+      print(response);
+    }
+    if (response != null && response.statusCode == 200) {
+      return userProfileDataFromJson(response.toString());
+    }
+    return null;
+  }
+}
