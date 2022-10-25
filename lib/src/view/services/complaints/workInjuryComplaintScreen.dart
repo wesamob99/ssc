4f3fd8793509891cda9f2f22e4c806e3e84b5f48
wesamobeidat <wesam.obe@hotmail.com>
2022-10-25 14:53:31 +0300

    update workInjuryComplaintScreen.dart

--- lib/src/view/services/complaints/workInjuryComplaintScreen.dart
+++ lib/src/view/services/complaints/workInjuryComplaintScreen.dart
@@ -3,11 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:group_radio_button/group_radio_button.dart';
 import 'package:provider/provider.dart';
+import 'package:ssc/src/view/services/components/firstStepScreen.dart';
 import 'package:ssc/src/viewModel/services/servicesProvider.dart';
 import 'package:ssc/utilities/util.dart';
 import 'dart:math' as math;
 import '../../../../infrastructure/userConfig.dart';
-import '../../../../models/profile/userProfileData.dart';
 import '../../../../utilities/hexColor.dart';
 import '../../../../utilities/theme/themes.dart';
 import '../../../viewModel/utilities/theme/themeProvider.dart';
@@ -21,13 +21,12 @@ class WorkInjuryComplaintScreen extends StatefulWidget {
 
 class _WorkInjuryComplaintScreenState extends State<WorkInjuryComplaintScreen> {
   ServicesProvider servicesProvider;
-  Future accountDataFuture;
 
   @override
   void initState() {
     servicesProvider = Provider.of<ServicesProvider>(context, listen: false);
-    accountDataFuture = servicesProvider.getAccountData();
     servicesProvider.stepNumber = 1;
+    servicesProvider.readCountriesJson();
     servicesProvider.selectedInjuredType = 'occupationalDisease';
     super.initState();
   }
@@ -72,7 +71,7 @@ class _WorkInjuryComplaintScreenState extends State<WorkInjuryComplaintScreen> {
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               if(Provider.of<ServicesProvider>(context).stepNumber == 1)
-                firstStep(context, themeNotifier, accountDataFuture, servicesProvider),
+                const FirstStepScreen(),
               if(Provider.of<ServicesProvider>(context).stepNumber == 2)
                 secondStep(context, themeNotifier, servicesProvider),
               if(Provider.of<ServicesProvider>(context).stepNumber == 3)
@@ -111,183 +110,6 @@ class _WorkInjuryComplaintScreenState extends State<WorkInjuryComplaintScreen> {
       ),
     );
   }
-}
-
-Widget firstStep(context, themeNotifier, accountDataFuture, ServicesProvider servicesProvider){
-  return SingleChildScrollView(
-    child: SizedBox(
-      height: height(0.7, context),
-      child: FutureBuilder(
-          future: accountDataFuture,
-          builder: (context, snapshot){
-            switch(snapshot.connectionState){
-              case ConnectionState.none:
-                return somethingWrongWidget(context, 'somethingWrongHappened', 'somethingWrongHappenedDesc'); break;
-              case ConnectionState.waiting:
-              case ConnectionState.active:
-                return Container(
-                    height: height(0.7, context),
-                    alignment: Alignment.center,
-                    child: animatedLoader(context)
-                ); break;
-              case ConnectionState.done:
-                if(snapshot.hasData && !snapshot.hasError){
-                  UserProfileData userProfileData = snapshot.data;
-                  CurGetdatum data = userProfileData.curGetdata[0][0];
-                  String name = '${data.firstname} ${data.fathername} ${data.grandfathername} ${data.familyname}';
-                  String natId = data.userName.toString();
-                  String insuranceNo = data.insuranceno.toString();
-                  String mobileNo = data.mobilenumber.toString();
-                  String internationalCode = data.internationalcode.toString();
-                  return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      SizedBox(height: height(0.02, context),),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            translate('firstStep', context),
-                            style: TextStyle(
-                                color: HexColor('#979797'),
-                                fontSize: width(0.03, context)
-                            ),
-                          ),
-                          SizedBox(height: height(0.006, context),),
-                          Text(
-                            translate('confirmPersonalInformation', context),
-                            style: TextStyle(
-                                color: HexColor('#5F5F5F'),
-                                fontSize: width(0.035, context)
-                            ),
-                          )
-                        ],
-                      ),
-                      SizedBox(height: height(0.01, context),),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          const SizedBox.shrink(),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.start,
-                            crossAxisAlignment: CrossAxisAlignment.end,
-                            children: [
-                              Text(
-                                '1/4',
-                                style: TextStyle(
-                                    color: HexColor('#979797'),
-                                    fontSize: width(0.025, context)
-                                ),
-                              ),
-                              Text(
-                                '${translate('next', context)}: ${translate('orderDetails', context)}',
-                                style: TextStyle(
-                                    color: HexColor('#979797'),
-                                    fontSize: width(0.032, context)
-                                ),
-                              ),
-                            ],
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: height(0.02, context),),
-                      Text(
-                        translate('quatrainNoun', context),
-                        style: TextStyle(
-                            color: HexColor('#363636'),
-                            fontSize: width(0.032, context)
-                        ),
-                      ),
-                      SizedBox(height: height(0.015, context),),
-                      buildTextFormField(context, themeNotifier, TextEditingController(text: name), '', (val){}, enabled: false),
-                      SizedBox(height: height(0.015, context),),
-                      Text(
-                        translate('nationalId', context),
-                        style: TextStyle(
-                            color: HexColor('#363636'),
-                            fontSize: width(0.032, context)
-                        ),
-                      ),
-                      SizedBox(height: height(0.015, context),),
-                      buildTextFormField(context, themeNotifier, TextEditingController(text: natId), '', (val){}, enabled: false),
-                      SizedBox(height: height(0.015, context),),
-                      Text(
-                        translate('securityNumber', context),
-                        style: TextStyle(
-                            color: HexColor('#363636'),
-                            fontSize: width(0.032, context)
-                        ),
-                      ),
-                      SizedBox(height: height(0.015, context),),
-                      buildTextFormField(context, themeNotifier, TextEditingController(text: insuranceNo), '', (val){}, enabled: false),
-                      SizedBox(height: height(0.015, context),),
-                      Text(
-                        translate('mobileNumber', context),
-                        style: TextStyle(
-                            color: HexColor('#363636'),
-                            fontSize: width(0.032, context)
-                        ),
-                      ),
-                      SizedBox(height: height(0.015, context),),
-                      Row(
-                        children: [
-                          Expanded(
-                              flex: 3,
-                              child: buildTextFormField(context, themeNotifier, TextEditingController(text: mobileNo), '', (val){})
-                          ),
-                          SizedBox(width: width(0.015, context)),
-                          Expanded(
-                              flex: 1,
-                              child: buildTextFormField(context, themeNotifier, TextEditingController(text: internationalCode), '', (val){})
-                          ),
-                        ],
-                      )
-                      // IntlPhoneField(
-                      //   controller: servicesProvider.mobileNumberController,
-                      //   disableLengthCheck: true,
-                      //   cursorColor: getPrimaryColor(context, themeNotifier),
-                      //   cursorWidth: 1,
-                      //   decoration: InputDecoration(
-                      //     prefixIconColor: Colors.red,
-                      //     contentPadding: const EdgeInsets.symmetric(horizontal: 16),
-                      //       errorText: null,
-                      //       errorBorder: OutlineInputBorder(
-                      //         borderRadius: BorderRadius.circular(8),
-                      //         borderSide: BorderSide(
-                      //           color: getPrimaryColor(context, themeNotifier),
-                      //           width: 0.5,
-                      //         ),
-                      //       ),
-                      //       border: OutlineInputBorder(
-                      //         borderRadius: BorderRadius.circular(8),
-                      //         borderSide: BorderSide(
-                      //           color: getPrimaryColor(context, themeNotifier),
-                      //           width: 0.5,
-                      //         ),
-                      //       ),
-                      //       focusedBorder: OutlineInputBorder(
-                      //         borderRadius: BorderRadius.circular(8),
-                      //         borderSide: BorderSide(
-                      //           color: getPrimaryColor(context, themeNotifier),
-                      //           width: 0.8,
-                      //         ),
-                      //       )
-                      //   ),
-                      //   initialCountryCode: 'JO',
-                      //   onChanged: (phone) {
-                      //     print(phone.completeNumber);
-                      //   },
-                      // )
-                    ],
-                  );
-                }
-                break;
-            }
-            return somethingWrongWidget(context, 'somethingWrongHappened', 'somethingWrongHappenedDesc');
-          }
-      ),
-    ),
-  );
 }
 
 Widget secondStep(context, themeNotifier, ServicesProvider servicesProvider){
