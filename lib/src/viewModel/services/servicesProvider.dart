18ed79e281835a014415be362d9eaeb1661393f7
wesamobeidat <wesam.obe@hotmail.com>
2022-10-25 14:49:17 +0300

    update servicesProvider.dart

--- lib/src/viewModel/services/servicesProvider.dart
+++ lib/src/viewModel/services/servicesProvider.dart
@@ -1,8 +1,10 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/services.dart';
 import 'package:ssc/src/model/services/servicesRepository.dart';
 
+import '../../../models/login/countries.dart';
 import '../../../models/profile/userProfileData.dart';
 
 class ServicesProvider extends ChangeNotifier {
@@ -14,12 +16,21 @@ class ServicesProvider extends ChangeNotifier {
   int stepNumber = 1;
   String selectedInjuredType = 'occupationalDisease';
   TextEditingController mobileNumberController = TextEditingController();
+  List<Countries> countries = [];
 
   /// workInjuryComplaint
   Future<UserProfileData> getAccountData() async{
     userProfileData = await servicesRepository.getAccountDataService();
     notifyMe();
     return userProfileData;
+  }
+
+  Future<void> readCountriesJson() async {
+    countries = [];
+    final String response = await rootBundle.loadString('assets/jsonFiles/countries.json');
+    countries = countriesFromJson(response);
+    notifyListeners();
+    print(countries);
   }
 
   void notifyMe() {
