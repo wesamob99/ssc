9e5f42a36ed13768417edc5f97b721047c057a18
wesamobeidat <wesam.obe@hotmail.com>
2022-10-26 10:10:36 +0300

    update homeLoaderWidget.dart

--- lib/src/view/home/components/homeLoaderWidget.dart
+++ lib/src/view/home/components/homeLoaderWidget.dart
@@ -70,6 +70,43 @@ class HomeLoaderWidget extends StatelessWidget {
           margin: const EdgeInsets.only(bottom: 10),
           cardLoadingTheme: cardLoadingTheme,
         ),
+        SizedBox(
+          height: height(0.1, context),
+          child: ListView.builder(
+            physics: const NeverScrollableScrollPhysics(),
+            itemCount: 7,
+            scrollDirection: Axis.horizontal,
+            itemBuilder: (context, index){
+              return Row(
+                children: [
+                  InkWell(
+                    onTap: (){},
+                    highlightColor: const Color.fromRGBO(68, 87, 64, 0.4),
+                    child: Column(
+                      children: [
+                        CardLoading(
+                          width: width(0.14, context),
+                          height: width(0.14, context),
+                          borderRadius: const BorderRadius.all(Radius.circular(10)),
+                          margin: const EdgeInsets.only(bottom: 10),
+                          cardLoadingTheme: cardLoadingTheme,
+                        ),
+                        CardLoading(
+                          width: width(0.14, context),
+                          height: width(0.01, context),
+                          borderRadius: const BorderRadius.all(Radius.circular(10)),
+                          margin: const EdgeInsets.only(bottom: 10),
+                          cardLoadingTheme: cardLoadingTheme,
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: width(0.015, context))
+                ],
+              );
+            }
+          ),
+        ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
