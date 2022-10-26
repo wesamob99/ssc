be980942d020dea0e8c52736e609eaff221049a9
wesamobeidat <wesam.obe@hotmail.com>
2022-10-26 10:34:47 +0300

    update homeLoaderWidget.dart

--- lib/src/view/home/components/homeLoaderWidget.dart
+++ lib/src/view/home/components/homeLoaderWidget.dart
@@ -70,42 +70,12 @@ class HomeLoaderWidget extends StatelessWidget {
           margin: const EdgeInsets.only(bottom: 10),
           cardLoadingTheme: cardLoadingTheme,
         ),
-        SizedBox(
-          height: height(0.1, context),
-          child: ListView.builder(
-            physics: const NeverScrollableScrollPhysics(),
-            itemCount: 7,
-            scrollDirection: Axis.horizontal,
-            itemBuilder: (context, index){
-              return Row(
-                children: [
-                  InkWell(
-                    onTap: (){},
-                    highlightColor: const Color.fromRGBO(68, 87, 64, 0.4),
-                    child: Column(
-                      children: [
-                        CardLoading(
-                          width: width(0.14, context),
-                          height: width(0.14, context),
-                          borderRadius: const BorderRadius.all(Radius.circular(10)),
-                          margin: const EdgeInsets.only(bottom: 10),
-                          cardLoadingTheme: cardLoadingTheme,
-                        ),
-                        CardLoading(
-                          width: width(0.14, context),
-                          height: width(0.01, context),
-                          borderRadius: const BorderRadius.all(Radius.circular(10)),
-                          margin: const EdgeInsets.only(bottom: 10),
-                          cardLoadingTheme: cardLoadingTheme,
-                        ),
-                      ],
-                    ),
-                  ),
-                  SizedBox(width: width(0.015, context))
-                ],
-              );
-            }
-          ),
+        CardLoading(
+          width: width(1, context),
+          height: width(0.14, context),
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+          margin: const EdgeInsets.only(bottom: 10),
+          cardLoadingTheme: cardLoadingTheme,
         ),
         Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
