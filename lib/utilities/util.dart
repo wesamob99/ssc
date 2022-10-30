9ec364b032370cbdae6a9bdb17f4ae785f9f22fa
wesamobeidat <wesam.obe@hotmail.com>
2022-10-30 13:05:43 +0300

    update animatedLoader size

--- lib/utilities/util.dart
+++ lib/utilities/util.dart
@@ -337,8 +337,8 @@ animatedLoader(context){
     elevation: 10.0,
     borderRadius: BorderRadius.circular(25.0),
   child: Container(
-      width: width(0.4, context),
-      height: width(0.4, context),
+      width: height(0.2, context),
+      height: height(0.2, context),
       decoration: BoxDecoration(
         color: Colors.transparent,
         borderRadius: BorderRadius.circular(25.0)
