d5b7f83eff3ef07b93b5ea2d78a5e76fefb87cde
wesamobeidat <wesam.obe@hotmail.com>
2022-10-31 11:17:00 +0300

    update

--- lib/src/view/services/shared/aboutTheServiceScreen.dart
+++ lib/src/view/services/shared/aboutTheServiceScreen.dart
@@ -36,7 +36,7 @@ class _AboutTheServiceScreenState extends State<AboutTheServiceScreen> {
   getTextStyle(context, isColored){
     return TextStyle(
         color: isColored ? HexColor('#003C97') : HexColor('#595959'),
-        fontSize: height(0.014, context)
+        fontSize: width(0.03, context)
     );
   }
 
@@ -75,19 +75,17 @@ class _AboutTheServiceScreenState extends State<AboutTheServiceScreen> {
           FocusScope.of(context).requestFocus(FocusNode());
         },
         child: Container(
+          height: height(1, context),
           width: width(1, context),
-          height: height(0.85, context),
           padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Stack(
             children: [
               SingleChildScrollView(
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Padding(
-                      padding: const EdgeInsets.only(bottom: 10),
+                      padding: const EdgeInsets.only(bottom: 5),
                       child: Text(
                         translate('aboutTheService', context),
                         style: const TextStyle(
@@ -97,24 +95,18 @@ class _AboutTheServiceScreenState extends State<AboutTheServiceScreen> {
                     ),
                     ReadMoreText(
                       widget.aboutServiceDescription,
-                      trimLines: 5,
+                      trimLines: 3,
                       colorClickableText: HexColor('#003C97'),
                       trimMode: TrimMode.Line,
                       trimCollapsedText: translate('readMore', context),
                       trimExpandedText: translate('readLess', context),
                       style: TextStyle(
-                        fontWeight: FontWeight.w100,
-                        height: 1.3,
-                        color: HexColor('#363636'),
+                          fontWeight: FontWeight.w100,
+                          height: 1.2,
+                          color: HexColor('#363636'),
+                          fontSize: height(0.018, context)
                       ),
                     ),
-                    // Text(
-                    //   widget.aboutServiceDescription,
-                    //   style: const TextStyle(
-                    //       fontWeight: FontWeight.w100,
-                    //       height: 1.3
-                    //   ),
-                    // ),
                     Divider(
                       indent: width(0.09, context),
                       endIndent: width(0.09, context),
@@ -123,7 +115,7 @@ class _AboutTheServiceScreenState extends State<AboutTheServiceScreen> {
                       height: height(0.04, context),
                     ),
                     Padding(
-                      padding: const EdgeInsets.only(bottom: 15),
+                      padding: const EdgeInsets.only(bottom: 10),
                       child: Text(
                         translate('termsOfTheService', context),
                         style: const TextStyle(
@@ -132,29 +124,39 @@ class _AboutTheServiceScreenState extends State<AboutTheServiceScreen> {
                       ),
                     ),
                     SizedBox(
-                      height: height(0.06, context) * widget.termsOfTheService.length,
+                      height: height(0.055, context) * widget.termsOfTheService.length,
                       child: ListView.builder(
                         physics: const NeverScrollableScrollPhysics(),
                           itemCount: widget.termsOfTheService.length,
                           itemBuilder: (context, index){
-                            return Row(
-                              children: [
-                                Card(
-                                  elevation: 5.0,
-                                  margin: const EdgeInsets.all(3.0),
-                                  shadowColor: const Color.fromRGBO(45, 69, 46, 0.28),
-                                  color: HexColor('#FFFFFF'),
-                                  child: Padding(
-                                    padding: EdgeInsets.all(width(0.025, context)),
-                                    child: SvgPicture.asset('assets/icons/quickAccessIcons/reportAnAccidentIcon.svg'),
+                            return SizedBox(
+                              height: height(0.055, context),
+                              child: Row(
+                                children: [
+                                  Card(
+                                    elevation: 5.0,
+                                    margin: const EdgeInsets.symmetric(vertical: 5.0).copyWith(bottom: 0),
+                                    shadowColor: const Color.fromRGBO(45, 69, 46, 0.28),
+                                    color: HexColor('#FFFFFF'),
+                                    child: Padding(
+                                      padding: EdgeInsets.all(height(0.01, context)),
+                                      child: SvgPicture.asset('assets/icons/quickAccessIcons/reportAnAccidentIcon.svg'),
+                                    ),
                                   ),
-                                ),
-                                SizedBox(width: width(0.03, context),),
-                                SizedBox(
-                                  width: width(0.74, context),
-                                  child: Text(widget.termsOfTheService[index]),
-                                )
-                              ],
+                                  SizedBox(width: width(0.03, context),),
+                                  SizedBox(
+                                    width: width(0.74, context),
+                                    child: Text(
+                                      widget.termsOfTheService[index],
+                                      maxLines: 2,
+                                      overflow: TextOverflow.ellipsis,
+                                      style: TextStyle(
+                                        fontSize: height(0.015, context),
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              ),
                             );
                           }
                       ),
@@ -167,20 +169,20 @@ class _AboutTheServiceScreenState extends State<AboutTheServiceScreen> {
                       height: height(0.04, context),
                     ),
                     Padding(
-                        padding: const EdgeInsets.only(bottom: 15),
+                        padding: const EdgeInsets.only(bottom: 10),
                         child: Row(
                           children: [
                             Text(
                               '${translate('stepsOfTheService', context)} ',
                               style: const TextStyle(
-                                  fontWeight: FontWeight.bold
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                             Text(
                               '( ${widget.stepsOfTheService.length} ${translate('steps', context)} )',
                               style: TextStyle(
-                                  fontWeight: FontWeight.w100,
-                                  color: HexColor('#666666')
+                                fontWeight: FontWeight.w100,
+                                color: HexColor('#666666'),
                               ),
                             ),
                           ],
@@ -192,10 +194,16 @@ class _AboutTheServiceScreenState extends State<AboutTheServiceScreen> {
                           physics: const NeverScrollableScrollPhysics(),
                           itemCount: widget.stepsOfTheService.length,
                           itemBuilder: (context, index){
-                            return Padding(
-                              padding: const EdgeInsets.only(bottom: 15.0),
-                              child: Text(
-                                '${index + 1}- ${widget.stepsOfTheService[index]}',
+                            return SizedBox(
+                              height: height(0.02, context) + 10.0,
+                              child: Padding(
+                                padding: const EdgeInsets.only(bottom: 10.0),
+                                child: Text(
+                                  '${index + 1}- ${widget.stepsOfTheService[index]}',
+                                  style: TextStyle(
+                                    fontSize: height(0.015, context),
+                                  ),
+                                ),
                               ),
                             );
                           }
@@ -207,67 +215,78 @@ class _AboutTheServiceScreenState extends State<AboutTheServiceScreen> {
                   ],
                 ),
               ),
-              Column(
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
+              Positioned(
+                bottom: 0,
+                child: Container(
+                  padding: const EdgeInsets.all(10.0),
+                  color: const Color.fromRGBO(253, 253, 253, 1.0),
+                  alignment: Alignment.center,
+                  width: width(1, context),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
-                      InkWell(
-                        onTap: (){
-                          setState(() {
-                            termsChecked = !termsChecked;
-                          });
-                        },
-                        child: Container(
-                          padding: const EdgeInsets.all(3.0),
-                          decoration: BoxDecoration(
-                              color: HexColor('#DADADA'),
-                              borderRadius: BorderRadius.circular(3.0)
-                          ),
-                          child: Container(
-                            width: width(0.04, context),
-                            height: width(0.04, context),
-                            decoration: BoxDecoration(
-                                color: termsChecked ? HexColor('#2D452E') : HexColor('#DADADA'),
-                                borderRadius: BorderRadius.circular(4.0)
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          InkWell(
+                            onTap: (){
+                              setState(() {
+                                termsChecked = !termsChecked;
+                              });
+                            },
+                            child: Container(
+                              padding: const EdgeInsets.all(3.0),
+                              decoration: BoxDecoration(
+                                  color: HexColor('#DADADA'),
+                                  borderRadius: BorderRadius.circular(3.0)
+                              ),
+                              child: Container(
+                                width: width(0.04, context),
+                                height: width(0.04, context),
+                                decoration: BoxDecoration(
+                                    color: termsChecked ? HexColor('#2D452E') : HexColor('#DADADA'),
+                                    borderRadius: BorderRadius.circular(4.0)
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                      SizedBox(width: width(0.05, context),),
-                      Column(
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.center,
+                          SizedBox(width: width(0.05, context),),
+                          Column(
                             children: [
-                              Text(translate('termsAndConditionsAndPoliciesAgreement11', context), style: getTextStyle(context, false),),
-                              Text(translate('termsAndConditionsAndPoliciesAgreement2', context), style: getTextStyle(context, true)),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(translate('termsAndConditionsAndPoliciesAgreement11', context), style: getTextStyle(context, false),),
+                                  Text(translate('termsAndConditionsAndPoliciesAgreement2', context), style: getTextStyle(context, true)),
+                                ],
+                              ),
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(translate('termsAndConditionsAndPoliciesAgreement3', context), style: getTextStyle(context, true)),
+                                  Text(translate('termsAndConditionsAndPoliciesAgreement4', context), style: getTextStyle(context, false))
+                                ],
+                              )
                             ],
                           ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(translate('termsAndConditionsAndPoliciesAgreement3', context), style: getTextStyle(context, true)),
-                              Text(translate('termsAndConditionsAndPoliciesAgreement4', context), style: getTextStyle(context, false))
-                            ],
-                          )
                         ],
+                      ),
+                      SizedBox(height: height(0.025, context),),
+                      textButton(
+                          context, themeNotifier, 'startNow', MaterialStateProperty.all<Color>(
+                          termsChecked ? getPrimaryColor(context, themeNotifier) : HexColor('#DADADA')),
+                          termsChecked ? HexColor('#ffffff') : HexColor('#363636'),
+                              () async {
+                            if(termsChecked){}
+                          }
                       ),
                     ],
                   ),
-                  SizedBox(height: height(0.025, context),),
-                  textButton(
-                    context, themeNotifier, 'startNow', MaterialStateProperty.all<Color>(
-                    termsChecked ? getPrimaryColor(context, themeNotifier) : HexColor('#DADADA')),
-                    termsChecked ? HexColor('#ffffff') : HexColor('#363636'),
-                    () async {
-                      if(termsChecked){}
-                    }
-                  ),
-                ],
-              ),
+                ),
+              )
             ],
           ),
         ),
@@ -275,3 +294,5 @@ class _AboutTheServiceScreenState extends State<AboutTheServiceScreen> {
     );
   }
 }
+
+
