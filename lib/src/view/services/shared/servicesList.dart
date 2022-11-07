14860ab67a08507ccbb1e4a558c8f661418c60b3
wesamobeidat <wesam.obe@hotmail.com>
2022-11-07 14:18:19 +0300

    create servicesList.dart (IMP) most important class

---
+++ lib/src/view/services/shared/servicesList.dart
@@ -0,0 +1,33 @@
+// import '../insuranceBenifits/screens/workInjuryComplaintScreen.dart';
+//
+// class ServicesList{
+//
+//   /// Quick Access Services ****************************************************
+//
+//   static const Map<String, dynamic> unemploymentApplication = {'icon': 'assets/icons/quickAccessIcons/unemploymentIcon.svg', 'title': "unemploymentApplication", "description": "this supposed to be about the service description",  "screen": WorkInjuryComplaintScreen()};
+//   static const Map<String, dynamic> onePayment = {'icon': 'assets/icons/quickAccessIcons/onePaymentIcon.svg', 'title': "onePayment", "description": "this supposed to be about the service description",  "screen": WorkInjuryComplaintScreen()};
+//   static const Map<String, dynamic> reportAnAccident = {'icon': 'assets/icons/quickAccessIcons/reportAnAccidentIcon.svg', 'title': "reportAnAccident", "description": "this supposed to be about the service description",  "screen": WorkInjuryComplaintScreen()};
+//   static const Map<String, dynamic> optionalSubscription = {'icon': 'assets/icons/quickAccessIcons/optionalSubscriptionIcon.svg', 'title': "optionalSubscription", "description": "this supposed to be about the service description",  "screen": WorkInjuryComplaintScreen()};
+//   static const Map<String, dynamic> accountStatement = {'icon': 'assets/icons/quickAccessIcons/accountStatementIcon.svg', 'title': "accountStatement", "description": "this supposed to be about the service description",  "screen": WorkInjuryComplaintScreen()};
+//
+//   List<Map<String, dynamic>> quickAccessServices = [unemploymentApplication, onePayment, reportAnAccident, optionalSubscription, accountStatement];
+//
+//   /// **************************************************************************
+//   /// Most Visited Services ****************************************************
+//
+//   static const Map<String, dynamic> requestRetiredLoan = {"title": "retired", "subTitle": "requestRetiredLoan", "description": "this supposed to be about the service description", "screen": WorkInjuryComplaintScreen()};
+//   static const Map<String, dynamic> unemploymentApplication = {"title": "individuals", "subTitle": "unemploymentApplication", "description": "this supposed to be about the service description", "screen": WorkInjuryComplaintScreen()};
+//   static const Map<String, dynamic> accountStatement = {"title": "maternity", "subTitle": "unemploymentApplication", "description": "this supposed to be about the service description", "screen": WorkInjuryComplaintScreen()};
+//   static const Map<String, dynamic> reportSicknessComplaint = {"title": "workInjuries", "subTitle": "report_a_sickness/work_injury_complaint", "description": "this supposed to be about the service description", "screen": WorkInjuryComplaintScreen()};
+//
+//   List<Map<String, dynamic>> mostVisitedServices = [requestRetiredLoan, unemploymentApplication, accountStatement, reportSicknessComplaint];
+//
+//   /// **************************************************************************
+//   /// Insurance Benefits Services **********************************************
+//
+//
+//
+//   /// **************************************************************************
+//
+//
+// }
