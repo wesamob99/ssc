239190855eb5aff9634b13eb82f7b74c7fb9932f
wesamobeidat <wesam.obe@hotmail.com>
2022-11-15 10:27:39 +0300

    update insuranceBody.dart

--- lib/src/view/services/insuranceBenefits/insuranceBody.dart
+++ lib/src/view/services/insuranceBenefits/insuranceBody.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: file_names
 
 import 'package:flutter/material.dart';
-import 'package:ssc/src/view/services/insuranceBenefits/screens/workInjuryComplaintScreen.dart';
 import 'package:ssc/src/view/services/shared/aboutTheServiceScreen.dart';
+import 'package:ssc/src/view/services/shared/servicesListConstants.dart';
 
 import '../../../../utilities/hexColor.dart';
 import '../../../../utilities/util.dart';
@@ -16,12 +16,7 @@ class InsuranceBody extends StatefulWidget {
 
 class _InsuranceBodyState extends State<InsuranceBody> {
 
-  List insuranceBenefitsServices = [
-    {"title": "retired", "subTitle": "requestRetiredLoan", "description": "this supposed to be about the service description", "screen": const WorkInjuryComplaintScreen()},
-    {"title": "individuals", "subTitle": "unemploymentApplication", "description": "this supposed to be about the service description", "screen": const WorkInjuryComplaintScreen()},
-    {"title": "maternity", "subTitle": "unemploymentApplication", "description": "this supposed to be about the service description", "screen": const WorkInjuryComplaintScreen()},
-    {"title": "workInjuries", "subTitle": "report_a_sickness/work_injury_complaint", "description": "this supposed to be about the service description", "screen": const WorkInjuryComplaintScreen()},
-  ];
+  List<Service> insuranceBenefitsServices = ServicesList.insuranceBenefitsServices;
 
   @override
   Widget build(BuildContext context) {
@@ -29,62 +24,63 @@ class _InsuranceBodyState extends State<InsuranceBody> {
       child: ListView.builder(
           itemCount: insuranceBenefitsServices.length,
           itemBuilder: (context, index){
-            return Container(
-              margin: EdgeInsets.only(bottom: height(0.02, context)),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  InkWell(
-                    onTap: (){
-                      Navigator.of(context).push(
-                        MaterialPageRoute(
-                          builder: (context) => AboutTheServiceScreen(
-                            serviceScreen: insuranceBenefitsServices[index]['screen'],
-                            serviceTitle: insuranceBenefitsServices[index]['subTitle'],
-                            aboutServiceDescription: ' بدلات التعطل عن العمل التي تقوم بصرفها والتي تخص المؤمن عليهم المشتركين بالضمان والعاملين في منشآت القطاع الخاص ممن يتعطّلون مؤقتاً عن العمل هي مبالغ غير مستردّة ولا يطالب المؤمن عليهم بإعادتها إلاّ في حال ثبت أن المؤمن عليه تقاضى أياً من هذه البدلات دون وجه حق إلاّ في حال ثبت أن المؤمن عليه تقاضى أياً من هذه البدلات إلاّ في حال ثبت أن المؤمن عليه تقاضى أياً من هذه البدلات',//mostVisitedServices[index]['description'],
-                            termsOfTheService: const [
-                              'موظفي القطاع الخاص',
-                              'موظف موقوف عن العمل',
-                              'لديك 36 اشتراك او رصيد اكثر من 300 د.ا',
-                              'ان تكون قد استفدت من بدل التعطل ثلاث مرات او اقل خلال فتره الشمول',
-                            ],
-                            stepsOfTheService: const [
-                              'التأكد من المعلومات الشخصية لمقدم الخدمة',
-                              'تعبئة طلب الخدمة',
-                              'تقديم الطلب'
-                            ],
-                          )
-                        ),
-                      );
-                    },
-                    child: Container(
-                      margin: const EdgeInsets.only(right: 10),
-                      width: width(1, context),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            translate(insuranceBenefitsServices[index]['title'], context),
-                            style: TextStyle(
-                                fontSize: width(isTablet(context) ? 0.03 : 0.035, context)
-                            ),
-                          ),
-                          SizedBox(height: height(0.006, context)),
-                          Text(
-                            translate(insuranceBenefitsServices[index]['subTitle'], context),
-                            style: TextStyle(
-                                fontSize: width(isTablet(context) ? 0.025 : 0.03, context)
-                            ),
-                          ),
-                        ],
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  onTap: (){
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => AboutTheServiceScreen(
+                          serviceScreen: insuranceBenefitsServices[index].screen,
+                          serviceTitle: insuranceBenefitsServices[index].title,
+                          aboutServiceDescription: ' بدلات التعطل عن العمل التي تقوم بصرفها والتي تخص المؤمن عليهم المشتركين بالضمان والعاملين في منشآت القطاع الخاص ممن يتعطّلون مؤقتاً عن العمل هي مبالغ غير مستردّة ولا يطالب المؤمن عليهم بإعادتها إلاّ في حال ثبت أن المؤمن عليه تقاضى أياً من هذه البدلات دون وجه حق إلاّ في حال ثبت أن المؤمن عليه تقاضى أياً من هذه البدلات إلاّ في حال ثبت أن المؤمن عليه تقاضى أياً من هذه البدلات',//mostVisitedServices[index]['description'],
+                          termsOfTheService: const [
+                            'موظفي القطاع الخاص',
+                            'موظف موقوف عن العمل',
+                            'لديك 36 اشتراك او رصيد اكثر من 300 د.ا',
+                            'ان تكون قد استفدت من بدل التعطل ثلاث مرات او اقل خلال فتره الشمول',
+                          ],
+                          stepsOfTheService: const [
+                            'التأكد من المعلومات الشخصية لمقدم الخدمة',
+                            'تعبئة طلب الخدمة',
+                            'تقديم الطلب'
+                          ],
+                        )
                       ),
+                    );
+                  },
+                  child: Container(
+                    margin: const EdgeInsets.only(right: 10),
+                    width: width(1, context),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          translate(insuranceBenefitsServices[index].supTitle, context),
+                          style: TextStyle(
+                              fontSize: width(isTablet(context) ? 0.03 : 0.035, context)
+                          ),
+                        ),
+                        SizedBox(height: height(0.006, context)),
+                        Text(
+                          translate(insuranceBenefitsServices[index].title, context),
+                          style: TextStyle(
+                              fontSize: width(isTablet(context) ? 0.025 : 0.03, context)
+                          ),
+                        ),
+                      ],
                     ),
                   ),
-                  Divider(
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 15.0),
+                  child: Divider(
                     color: HexColor('#DEDEDE'),
+                    thickness: 1,
                   ),
-                ],
-              ),
+                ),
+              ],
             );
           }
       ),
