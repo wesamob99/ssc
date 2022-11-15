63d39ba67b0fbe6e6e3e3273bea628ae72699efa
wesamobeidat <wesam.obe@hotmail.com>
2022-11-15 10:04:01 +0300

    servicesListConstants.dart update

--- lib/src/view/services/shared/servicesListConstants.dart
+++ lib/src/view/services/shared/servicesListConstants.dart
@@ -1,46 +1,57 @@
-// ignore_for_file: file_names
-// import 'package:flutter/cupertino.dart';
-//
-// import '../insuranceBenefits/screens/workInjuryComplaintScreen.dart';
-//
-// class ServicesList{
-//
-//   /// Quick Access Services ****************************************************
-//
-//   static const Map<String, dynamic> unemploymentApplication = {'icon': 'assets/icons/quickAccessIcons/unemploymentIcon.svg', 'title': "unemploymentApplication", "description": "this supposed to be about the service description",  "screen": WorkInjuryComplaintScreen()};
-//   static const Map<String, dynamic> onePayment = {'icon': 'assets/icons/quickAccessIcons/onePaymentIcon.svg', 'title': "onePayment", "description": "this supposed to be about the service description",  "screen": WorkInjuryComplaintScreen()};
-//   static const Map<String, dynamic> reportAnAccident = {'icon': 'assets/icons/quickAccessIcons/reportAnAccidentIcon.svg', 'title': "reportAnAccident", "description": "this supposed to be about the service description",  "screen": WorkInjuryComplaintScreen()};
-//   static const Map<String, dynamic> optionalSubscription = {'icon': 'assets/icons/quickAccessIcons/optionalSubscriptionIcon.svg', 'title': "optionalSubscription", "description": "this supposed to be about the service description",  "screen": WorkInjuryComplaintScreen()};
-//   static const Map<String, dynamic> accountStatement = {'icon': 'assets/icons/quickAccessIcons/accountStatementIcon.svg', 'title': "accountStatement", "description": "this supposed to be about the service description",  "screen": WorkInjuryComplaintScreen()};
-//
-//   List<Map<String, dynamic>> quickAccessServices = [unemploymentApplication, onePayment, reportAnAccident, optionalSubscription, accountStatement];
-//
-//   /// **************************************************************************
-//   /// Most Visited Services ****************************************************
-//
-//   static const Map<String, dynamic> requestRetiredLoan = {"title": "retired", "subTitle": "requestRetiredLoan", "description": "this supposed to be about the service description", "screen": WorkInjuryComplaintScreen()};
-//   static const Map<String, dynamic> unemploymentApplication = {"title": "individuals", "subTitle": "unemploymentApplication", "description": "this supposed to be about the service description", "screen": WorkInjuryComplaintScreen()};
-//   static const Map<String, dynamic> accountStatement = {"title": "maternity", "subTitle": "unemploymentApplication", "description": "this supposed to be about the service description", "screen": WorkInjuryComplaintScreen()};
-//   static const Map<String, dynamic> reportSicknessComplaint = {"title": 'workInjuries', "subTitle": "report_a_sickness/work_injury_complaint", "description": "this supposed to be about the service description", "screen": WorkInjuryComplaintScreen()};
-//
-//   List<Map<String, dynamic>> mostVisitedServices = [requestRetiredLoan, unemploymentApplication, accountStatement, reportSicknessComplaint];
-//
-//   /// **************************************************************************
-//   /// Insurance Benefits Services **********************************************
-//
-//
-//
-//   /// **************************************************************************
-//
-//
-// }
-//
-// class Service{
-//   final String title;
-//   final String subTitle;
-//   final String icon;
-//   final String description;
-//   final Widget screen;
-//
-//   const Service({this.title, this.subTitle, this.icon, this.description, this.screen});
-// }
+//ignore_for_file: file_names
+import 'package:flutter/cupertino.dart';
+
+import '../insuranceBenefits/screens/workInjuryComplaintScreen.dart';
+
+class ServicesList{
+
+  /// Quick Access Services ****************************************************
+
+  static const Service unemploymentApplication = Service(
+    title: "unemploymentApplication", supTitle: "individuals", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen: WorkInjuryComplaintScreen()
+  );
+  static const Service onePayment = Service(
+      title: "onePayment", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/onePaymentIcon.svg', screen: WorkInjuryComplaintScreen()
+  );
+  static const Service reportAnAccident = Service(
+      title: "reportAnAccident", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/reportAnAccidentIcon.svg', screen: WorkInjuryComplaintScreen()
+  );
+  static const Service optionalSubscription = Service(
+      title: "optionalSubscription", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/optionalSubscriptionIcon.svg', screen: WorkInjuryComplaintScreen()
+  );
+  static const Service accountStatement = Service(
+      title: "accountStatement", supTitle: "maternity", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/accountStatementIcon.svg', screen: WorkInjuryComplaintScreen()
+  );
+  List<Service> quickAccessServices = [unemploymentApplication, onePayment, reportAnAccident, optionalSubscription, accountStatement];
+
+  /// **************************************************************************
+  /// Most Visited Services ****************************************************
+
+  static const Service requestRetiredLoan = Service(
+      title: "requestRetiredLoan", supTitle: "retired", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen: WorkInjuryComplaintScreen()
+  );
+  static const Service reportSicknessComplaint = Service(
+      title: "report_a_sickness/work_injury_complaint", supTitle: "workInjuries", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen: WorkInjuryComplaintScreen()
+  );
+
+  List<Service> mostVisitedServices = [requestRetiredLoan, unemploymentApplication, accountStatement, reportSicknessComplaint];
+
+  /// **************************************************************************
+  /// Insurance Benefits Services **********************************************
+
+
+
+  /// **************************************************************************
+
+
+}
+
+class Service{
+  final String title;
+  final String supTitle;
+  final String icon;
+  final String description;
+  final Widget screen;
+
+  const Service({this.title, this.supTitle, this.icon, this.description, this.screen});
+}
