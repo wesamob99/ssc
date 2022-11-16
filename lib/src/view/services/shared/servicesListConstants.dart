ad6df384c6300bbb4f0d8f954728899a5a416e6a
wesamobeidat <wesam.obe@hotmail.com>
2022-11-16 12:14:54 +0300

    make servicesListConstants.dart items not constant

--- lib/src/view/services/shared/servicesListConstants.dart
+++ lib/src/view/services/shared/servicesListConstants.dart
@@ -7,51 +7,51 @@ class ServicesList{
 
   /// Quick Access Services ****************************************************
 
-  static const Service individualsUnemploymentApplication = Service(
-    title: "unemploymentApplication", supTitle: "individuals", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen: WorkInjuryComplaintScreen()
+  static Service individualsUnemploymentApplication = Service(
+    title: "unemploymentApplication", supTitle: "individuals", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen:  const WorkInjuryComplaintScreen()
   );
-  static const Service maternityUnemploymentApplication = Service(
-      title: "unemploymentApplication", supTitle: "maternity", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen: WorkInjuryComplaintScreen()
+  static Service maternityUnemploymentApplication = Service(
+      title: "unemploymentApplication", supTitle: "maternity", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen:  const WorkInjuryComplaintScreen()
   );
-  static const Service onePayment = Service(
-      title: "onePayment", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/onePaymentIcon.svg', screen: WorkInjuryComplaintScreen()
+  static Service onePayment = Service(
+      title: "onePayment", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/onePaymentIcon.svg', screen:  const WorkInjuryComplaintScreen()
   );
-  static const Service reportAnAccident = Service(
-      title: "reportAnAccident", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/reportAnAccidentIcon.svg', screen: WorkInjuryComplaintScreen()
+  static Service reportAnAccident = Service(
+      title: "reportAnAccident", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/reportAnAccidentIcon.svg', screen:  const WorkInjuryComplaintScreen()
   );
-  static const Service optionalSubscription = Service(
-      title: "optionalSubscription", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/optionalSubscriptionIcon.svg', screen: WorkInjuryComplaintScreen()
+  static Service optionalSubscription = Service(
+      title: "optionalSubscription", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/optionalSubscriptionIcon.svg', screen:  const WorkInjuryComplaintScreen()
   );
-  static const Service accountStatement = Service(
-      title: "accountStatement", supTitle: "maternity", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/accountStatementIcon.svg', screen: WorkInjuryComplaintScreen()
+  static Service accountStatement = Service(
+      title: "accountStatement", supTitle: "maternity", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/accountStatementIcon.svg', screen:  const WorkInjuryComplaintScreen()
   );
-  static const List<Service> quickAccessServices = [individualsUnemploymentApplication, onePayment, reportAnAccident, optionalSubscription, accountStatement];
+  static List<Service> quickAccessServices = [individualsUnemploymentApplication, onePayment, reportAnAccident, optionalSubscription, accountStatement];
 
   /// **************************************************************************
   /// Most Visited Services ****************************************************
 
-  static const Service requestRetiredLoan = Service(
-      title: "requestRetiredLoan", supTitle: "retired", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen: WorkInjuryComplaintScreen()
+  static Service requestRetiredLoan = Service(
+      title: "requestRetiredLoan", supTitle: "retired", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen:  const WorkInjuryComplaintScreen()
   );
-  static const Service reportSicknessComplaint = Service(
-      title: "report_a_sickness/work_injury_complaint", supTitle: "workInjuries", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen: WorkInjuryComplaintScreen()
+  static Service reportSicknessComplaint = Service(
+      title: "report_a_sickness/work_injury_complaint", supTitle: "workInjuries", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen:  const WorkInjuryComplaintScreen()
   );
 
-  static const List<Service> mostVisitedServices = [requestRetiredLoan, individualsUnemploymentApplication, maternityUnemploymentApplication];
+  static List<Service> mostVisitedServices = [requestRetiredLoan, individualsUnemploymentApplication, maternityUnemploymentApplication];
 
   /// **************************************************************************
   /// Insurance Benefits Services **********************************************
 
-  static const Service oneTimeCompensationRequest = Service(
-      title: "oneTimeCompensationRequest", supTitle: "compensation", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen: WorkInjuryComplaintScreen()
+  static Service oneTimeCompensationRequest = Service(
+      title: "oneTimeCompensationRequest", supTitle: "compensation", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen:  const WorkInjuryComplaintScreen()
   );
-  static const Service savingsBalanceDisbursementRequest = Service(
-      title: "savingsBalanceDisbursementRequest", supTitle: "savingsBalance", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen: WorkInjuryComplaintScreen()
+  static Service savingsBalanceDisbursementRequest = Service(
+      title: "savingsBalanceDisbursementRequest", supTitle: "savingsBalance", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen:  const WorkInjuryComplaintScreen()
   );
-  static const Service workInjuryInsurance = Service(
-      title: "workInjuryInsurance", supTitle: "workInjuries", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen: WorkInjuryComplaintScreen()
+  static Service workInjuryInsurance = Service(
+      title: "workInjuryInsurance", supTitle: "workInjuries", description: "this supposed to be about the service description", icon: 'assets/icons/quickAccessIcons/unemploymentIcon.svg', screen:  const WorkInjuryComplaintScreen()
   );
-  static const List<Service> insuranceBenefitsServices = [oneTimeCompensationRequest, savingsBalanceDisbursementRequest, reportSicknessComplaint, workInjuryInsurance];
+  static List<Service> insuranceBenefitsServices = [oneTimeCompensationRequest, savingsBalanceDisbursementRequest, reportSicknessComplaint, workInjuryInsurance];
 
   /// **************************************************************************
 
@@ -64,6 +64,7 @@ class Service{
   final String icon;
   final String description;
   final Widget screen;
+  bool isSelected;
 
-  const Service({this.title, this.supTitle, this.icon, this.description, this.screen});
+  Service({this.title, this.supTitle, this.icon, this.description, this.screen, this.isSelected = true});
 }
