cf3931479658a115695af53407710bbb896b43fd
wesamobeidat <wesam.obe@hotmail.com>
2022-11-16 12:33:33 +0300

    update quickAccessWidget.dart to be customizable

--- lib/src/view/home/components/quickAccessWidget.dart
+++ lib/src/view/home/components/quickAccessWidget.dart
@@ -2,18 +2,25 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
 import 'package:ssc/src/view/services/shared/aboutTheServiceScreen.dart';
 import 'package:ssc/src/view/services/shared/servicesListConstants.dart';
+import 'package:ssc/src/viewModel/home/homeProvider.dart';
 
 import '../../../../utilities/hexColor.dart';
 import '../../../../utilities/util.dart';
 
-class QuickAccessWidget extends StatelessWidget {
+class QuickAccessWidget extends StatefulWidget {
   const QuickAccessWidget({Key key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  State<QuickAccessWidget> createState() => _QuickAccessWidgetState();
+}
 
+class _QuickAccessWidgetState extends State<QuickAccessWidget> {
+  @override
+  Widget build(BuildContext context) {
+    HomeProvider homeProviderListener = Provider.of<HomeProvider>(context);
     List<Service> quickAccessServices = ServicesList.quickAccessServices;
 
     return SizedBox(
@@ -22,69 +29,98 @@ class QuickAccessWidget extends StatelessWidget {
           itemCount: quickAccessServices.length,
           scrollDirection: Axis.horizontal,
           itemBuilder: (context, index){
-            return Row(
-              children: [
-                InkWell(
-                  onTap: (){
-                    Navigator.of(context).push(
-                      MaterialPageRoute(
-                        builder: (context) => AboutTheServiceScreen(
-                          serviceScreen: quickAccessServices[index].screen,
-                          serviceTitle: quickAccessServices[index].title,
-                          aboutServiceDescription: quickAccessServices[index].description,
-                          termsOfTheService: const [
-                            'موظفي القطاع الخاص',
-                            'موظف موقوف عن العمل',
-                            'لديك 36 اشتراك او رصيد اكثر من 300 د.ا',
-                            'ان تكون قد استفدت من بدل التعطل ثلاث مرات او اقل خلال فتره الشمول',
-                          ],
-                          stepsOfTheService: const [
-                            'التأكد من المعلومات الشخصية لمقدم الخدمة',
-                            'تعبئة طلب الخدمة',
-                            'تقديم الطلب'
+            return quickAccessServices[index].isSelected || (!quickAccessServices[index].isSelected && homeProviderListener.isEditQuickAccessActive)
+            ? Row(
+                children: [
+                  InkWell(
+                    onTap: (){
+                      if(!homeProviderListener.isEditQuickAccessActive) {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => AboutTheServiceScreen(
+                              serviceScreen: quickAccessServices[index].screen,
+                              serviceTitle: quickAccessServices[index].title,
+                              aboutServiceDescription: quickAccessServices[index].description,
+                              termsOfTheService: const [
+                                'موظفي القطاع الخاص',
+                                'موظف موقوف عن العمل',
+                                'لديك 36 اشتراك او رصيد اكثر من 300 د.ا',
+                                'ان تكون قد استفدت من بدل التعطل ثلاث مرات او اقل خلال فتره الشمول',
+                              ],
+                              stepsOfTheService: const [
+                                'التأكد من المعلومات الشخصية لمقدم الخدمة',
+                                'تعبئة طلب الخدمة',
+                                'تقديم الطلب'
+                              ],
+                            ),
+                          ),
+                        );
+                      } else{
+                        setState((){
+                          quickAccessServices[index].isSelected = !quickAccessServices[index].isSelected;
+                        });
+                      }
+                    },
+                    highlightColor: const Color.fromRGBO(68, 87, 64, 0.4),
+                    child: Column(
+                      children: [
+                        Stack(
+                          children: [
+                            Card(
+                                elevation: 5.0,
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(6.0),
+                                ),
+                                margin: EdgeInsets.symmetric(vertical: 3.0, horizontal: isTablet(context) ? 5.0 : 0),
+                                shadowColor: const Color.fromRGBO(45, 69, 46, 0.28),
+                                color: HexColor('#FFFFFF'),
+                                child: Padding(
+                                  padding: EdgeInsets.all(isTablet(context) ? 17.0 : 14.0),
+                                  child: SvgPicture.asset(
+                                    quickAccessServices[index].icon,
+                                    color: !quickAccessServices[index].isSelected ? Colors.black26 : HexColor('#946800'),
+                                    width: isTablet(context) ? 48 : 32,
+                                    height: isTablet(context) ? 48 : 32,
+                                  ),
+                                )
+                            ),
+                            if(homeProviderListener.isEditQuickAccessActive)
+                              Container(
+                                margin: const EdgeInsets.all(5.0).copyWith(top: 7.0),
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    borderRadius: BorderRadius.circular(25.0),
+                                    border: Border.all(
+                                      color: quickAccessServices[index].isSelected ? HexColor('#BC0D0D') : HexColor('#003C97'),
+                                    )
+                                ),
+                                child: Icon(
+                                  quickAccessServices[index].isSelected ? Icons.remove : Icons.add,
+                                  color: quickAccessServices[index].isSelected ? HexColor('#BC0D0D') : HexColor('#003C97'),
+                                  size: isTablet(context) ? 36 : 20,
+                                ),
+                              ),
                           ],
                         ),
-                      ),
-                    );
-                  },
-                  highlightColor: const Color.fromRGBO(68, 87, 64, 0.4),
-                  child: Column(
-                    children: [
-                      Card(
-                        elevation: 5.0,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6.0),
-                        ),
-                        margin: EdgeInsets.symmetric(vertical: 3.0, horizontal: isTablet(context) ? 5.0 : 0),
-                        shadowColor: const Color.fromRGBO(45, 69, 46, 0.28),
-                        color: HexColor('#FFFFFF'),
-                        child: Padding(
-                          padding: EdgeInsets.all(isTablet(context) ? 17.0 : 14.0),
-                          child: SvgPicture.asset(
-                            quickAccessServices[index].icon,
-                            width: isTablet(context) ? 48 : 32,
-                            height: isTablet(context) ? 48 : 32,
+                        SizedBox(
+                          width: isTablet(context) ? 90.0 : 65.0,
+                          child: Text(
+                            translate(quickAccessServices[index].title, context),
+                            textAlign: TextAlign.center,
+                            maxLines: isScreenHasSmallHeight(context) ? 2 : 3,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: height(0.012, context),
+                            ),
                           ),
                         ),
-                      ),
-                      SizedBox(
-                        width: isTablet(context) ? 90.0 : 65.0,
-                        child: Text(
-                          translate(quickAccessServices[index].title, context),
-                          textAlign: TextAlign.center,
-                          maxLines: isScreenHasSmallHeight(context) ? 2 : 3,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                            fontSize: height(0.012, context),
-                          ),
-                        ),
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
-                SizedBox(width: width(index != quickAccessServices.length-1 ? 0.006 : 0, context))
-              ],
-            );
+                  SizedBox(width: width(index != quickAccessServices.length-1 ? 0.006 : 0, context))
+                ],
+              )
+            : const SizedBox.shrink();
           }
       ),
     );
