0eecf0a6a9fb2149a6a4d32a7f11afc15eca8fd7
wesamobeidat <wesam.obe@hotmail.com>
2022-11-17 10:21:34 +0300

    UI update on all services bodies

--- lib/src/view/services/optionalAndFreeInclusion/optionalAndFreeInclusionBody.dart
+++ lib/src/view/services/optionalAndFreeInclusion/optionalAndFreeInclusionBody.dart
@@ -65,48 +65,51 @@ class _OptionalAndFreeInclusionBodyState extends State<OptionalAndFreeInclusionB
                   itemCount: optionalAndFreeInclusionServices.length,
                   itemBuilder: (context, index2){
                     return optionalAndFreeInclusionServices[index2].supTitle == supTitles[index1]
-                      ? Column(
-                      children: [
-                        InkWell(
-                          onTap: (){
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                  builder: (context) => AboutTheServiceScreen(
-                                    serviceScreen: optionalAndFreeInclusionServices[index2].screen,
-                                    serviceTitle: optionalAndFreeInclusionServices[index2].title,
-                                    aboutServiceDescription: optionalAndFreeInclusionServices[index2].description,
-                                    termsOfTheService: const [
-                                      'موظفي القطاع الخاص',
-                                      'موظف موقوف عن العمل',
-                                      'لديك 36 اشتراك او رصيد اكثر من 300 د.ا',
-                                      'ان تكون قد استفدت من بدل التعطل ثلاث مرات او اقل خلال فتره الشمول',
-                                    ],
-                                    stepsOfTheService: const [
-                                      'التأكد من المعلومات الشخصية لمقدم الخدمة',
-                                      'تعبئة طلب الخدمة',
-                                      'تقديم الطلب'
-                                    ],
-                                  )
-                              ),
-                            );
-                          },
-                          child: Container(
-                            margin: EdgeInsets.only(right: 20.0, top: 10.0, bottom: (last != -1 && last != index2) ? 15.0 : 25.0),
-                            width: width(1, context),
-                            child: Text(
-                              translate(optionalAndFreeInclusionServices[index2].title, context),
-                              style: TextStyle(
-                                  fontSize: width(isTablet(context) ? 0.025 : 0.03, context)
+                    ? Padding(
+                      padding: const EdgeInsets.only(left: 18.0),
+                      child: Column(
+                        children: [
+                          InkWell(
+                            onTap: (){
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                    builder: (context) => AboutTheServiceScreen(
+                                      serviceScreen: optionalAndFreeInclusionServices[index2].screen,
+                                      serviceTitle: optionalAndFreeInclusionServices[index2].title,
+                                      aboutServiceDescription: optionalAndFreeInclusionServices[index2].description,
+                                      termsOfTheService: const [
+                                        'موظفي القطاع الخاص',
+                                        'موظف موقوف عن العمل',
+                                        'لديك 36 اشتراك او رصيد اكثر من 300 د.ا',
+                                        'ان تكون قد استفدت من بدل التعطل ثلاث مرات او اقل خلال فتره الشمول',
+                                      ],
+                                      stepsOfTheService: const [
+                                        'التأكد من المعلومات الشخصية لمقدم الخدمة',
+                                        'تعبئة طلب الخدمة',
+                                        'تقديم الطلب'
+                                      ],
+                                    )
+                                ),
+                              );
+                            },
+                            child: Container(
+                              margin: EdgeInsets.only(right: 20.0, top: 10.0, bottom: (last != -1 && last != index2) ? 15.0 : 25.0),
+                              width: width(1, context),
+                              child: Text(
+                                translate(optionalAndFreeInclusionServices[index2].title, context),
+                                style: TextStyle(
+                                    fontSize: width(isTablet(context) ? 0.025 : 0.03, context)
+                                ),
                               ),
                             ),
                           ),
-                        ),
-                        if(last != -1 && last != index2)
-                          Divider(
-                            color: HexColor('#DEDEDE'),
-                            thickness: 1,
-                          ),
-                      ],
+                          if(last != -1 && last != index2)
+                            Divider(
+                              color: HexColor('#DEDEDE'),
+                              thickness: 1,
+                            ),
+                        ],
+                      ),
                     ) : const SizedBox.shrink();
                   },
                 ),
