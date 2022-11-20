824cb8139eaf7ad08b9e81b40fddc0c113291ba5
wesamobeidat <wesam.obe@hotmail.com>
2022-11-20 12:48:44 +0300

    update

--- lib/src/view/services/insuranceServices/insuranceBody.dart
+++ lib/src/view/services/insuranceServices/insuranceBody.dart
@@ -110,6 +110,7 @@ class _InsuranceBodyState extends State<InsuranceBody> {
                     ) : const SizedBox.shrink();
                   },
                 ),
+                SizedBox(height: isTablet(context) ? 25 : 5,)
               ],
             );
           }
