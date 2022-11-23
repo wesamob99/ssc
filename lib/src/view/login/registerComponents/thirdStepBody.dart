4512b1764c4dc55ab072a3923309d4e558b5a293
wesamobeidat <wesam.obe@hotmail.com>
2022-11-23 12:13:34 +0300

    update thirdStepBody.dart's registerData

--- lib/src/view/login/registerComponents/thirdStepBody.dart
+++ lib/src/view/login/registerComponents/thirdStepBody.dart
@@ -224,7 +224,8 @@ class _ThirdStepBodyState extends State<ThirdStepBody> {
                         ? HexColor('#ffffff') : HexColor('#363636'), () async {
                       if(loginProvider.registerContinueEnabled){
                         loginProvider.registerData.email = item1['value'] ? loginProvider.emailController.text : null;
-                        loginProvider.registerData.activationBy = 1; //activationBy
+                        /// TODO: check updates about activationBy in case email is selected
+                        loginProvider.registerData.activationBy = 3; //activationBy
                         loginProvider.notifyMe();
                         if(item1['value']) {
                           FocusScope.of(context).requestFocus(FocusNode());
