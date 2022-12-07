f3bc5b0492451ae59a36194d9b1bff39e27f23d7
wesamobeidat <wesam.obe@hotmail.com>
2022-12-07 09:02:03 +0300

    create payScreen.dart

---
+++ lib/src/view/pay/payScreen.dart
@@ -0,0 +1,18 @@
+
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class PayScreen extends StatefulWidget {
+  const PayScreen({Key key}) : super(key: key);
+
+  @override
+  State<PayScreen> createState() => _PayScreenState();
+}
+
+class _PayScreenState extends State<PayScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
