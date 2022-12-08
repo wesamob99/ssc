88dd5b0d3107b5f19e0159180821e604bee8345b
wesamobeidat <wesam.obe@hotmail.com>
2022-12-08 15:07:25 +0300

    fully update paymentMethodsScreen.dart

--- lib/src/view/pay/paymentMethodsScreen.dart
+++ lib/src/view/pay/paymentMethodsScreen.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:ssc/utilities/hexColor.dart';
+import 'package:ssc/utilities/theme/themes.dart';
 
 import '../../../utilities/util.dart';
 
 class PaymentMethodsScreen extends StatefulWidget {
-  const PaymentMethodsScreen({Key key}) : super(key: key);
+  final String payCode;
+  const PaymentMethodsScreen({Key key, @required this.payCode}) : super(key: key);
 
   @override
   State<PaymentMethodsScreen> createState() => _PaymentMethodsScreenState();
 }
 
 class _PaymentMethodsScreenState extends State<PaymentMethodsScreen> {
+
+  List methods = [
+    {"name": "bank", "icon": "assets/icons/bank.svg", "isSelected": true},
+    {"name": "eFawateercom", "icon": "assets/icons/eFawateercom.svg", "isSelected": false},
+    {"name": "cliq", "icon": "assets/icons/eFawateercom.svg", "isSelected": false},
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,7 +30,154 @@ class _PaymentMethodsScreenState extends State<PaymentMethodsScreen> {
         title: Text(translate('paymentMethods', context)),
         leading: leadingBackIcon(context),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: width(0.26, context),
+              width: width(1, context),
+              child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                itemCount: methods.length,
+                itemBuilder: (context, index){
+                  return Row(
+                    children: [
+                      InkWell(
+                        onTap: (){
+                          for (var element in methods) {
+                            if(element == methods[index]){
+                              setState(() {
+                                element['isSelected'] = true;
+                              });
+                            } else{
+                              setState(() {
+                                element['isSelected'] = false;
+                              });
+                            }
+                          }
+                        },
+                        splashColor: primaryColor.withOpacity(0.3),
+                        highlightColor: Colors.transparent,
+                        child: AnimatedContainer(
+                          duration: const Duration(milliseconds: 300),
+                          width: width(0.23, context),
+                          height: width(0.23, context),
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8.0),
+                              border: Border.all(
+                                color: methods[index]['isSelected'] ? HexColor('#445740') : HexColor('#C9C9C9'),
+                              )
+                          ),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              SvgPicture.asset(
+                                methods[index]['icon'], width: width(0.09, context), height: width(0.09, context),
+                              ),
+                              const SizedBox(height: 10.0),
+                              Text(
+                                translate(methods[index]['name'], context),
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                  color: methods[index]['isSelected'] ? HexColor('#363636') : HexColor('#B3B3B3'),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                      ),
+                      const SizedBox(width: 10),
+                    ],
+                  );
+                }
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: height(0.05, context), bottom: height(0.035, context)),
+              child: Text(
+                translate('steps', context),
+                style: const TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.w600
+                ),
+              ),
+            ),
+            if(methods[0]['isSelected'])
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                      padding: EdgeInsets.only(bottom: height(0.012, context)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            translate('bankStep_1', context),
+                          ),
+                          const SizedBox(height: 10.0),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Container(
+                                padding: const EdgeInsets.symmetric(horizontal: 15.0, vertical: 10.0),
+                                decoration: BoxDecoration(
+                                    border: Border.all(
+                                      color: HexColor('#445740'),
+                                    ),
+                                    borderRadius: BorderRadius.circular(12.0)
+                                ),
+                                child: Text(
+                                  widget.payCode,
+                                  style: TextStyle(
+                                    color: HexColor('#363636'),
+                                    fontSize: width(isTablet(context) ? 0.036 : 0.04, context),
+                                  ),
+                                ),
+                              ),
+                              const SizedBox(width: 5.0),
+                              TextButton.icon(
+                                onPressed: () async{
+                                  await Clipboard.setData(
+                                      ClipboardData(text: widget.payCode)
+                                  );
+                                },
+                                icon: Icon(
+                                  Icons.copy,
+                                  color: HexColor('#003C97'),
+                                ),
+                                label: Text(
+                                  translate('copy', context),
+                                  style: TextStyle(
+                                      color: HexColor('#003C97'),
+                                      decoration: TextDecoration.underline
+                                  ),
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      )
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: height(0.024, context)),
+                    child: Text(
+                      translate('bankStep_2', context),
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: height(0.024, context)),
+                    child: Text(
+                      translate('bankStep_3', context),
+                    ),
+                  ),
+                ],
+              )
+          ],
+        ),
+      ),
     );
   }
 }
