8af78051ad2fbe2cbeea2cafbe2961b1c8bf2dca
wesamobeidat <wesam.obe@hotmail.com>
2022-12-08 10:59:00 +0300

    create issuanceOfUnifiedPaymentCodeService in payProvider.dart & payRepository.dart

--- lib/src/model/pay/payRepository.dart
+++ lib/src/model/pay/payRepository.dart
@@ -1,5 +1,24 @@
 // ignore_for_file: file_names
 
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+
+import '../../../infrastructure/HTTPClientContract.dart';
+
 class PayRepository{
 
+  Future issuanceOfUnifiedPaymentCodeService(List<String> data) async {
+    var response = await HTTPClientContract.instance.postHTTP(
+        '/individuals/SAVE_TEMP_DATA',
+        {"XML":"$data"}
+    );
+    if (kDebugMode) {
+      print(response);
+    }
+    if (response != null && response.statusCode == 200) {
+      return jsonDecode(response.data);
+    }
+    return '';
+  }
 }
--- lib/src/viewModel/pay/payProvider.dart
+++ lib/src/viewModel/pay/payProvider.dart
@@ -2,7 +2,37 @@
 
 import 'package:flutter/cupertino.dart';
 
+import '../../../models/home/payOffFinancialInformations.dart';
+import '../../model/pay/payRepository.dart';
+
 class PayProvider extends ChangeNotifier {
+
+  PayRepository payRepository = PayRepository();
+
+  Future issuanceOfUnifiedPaymentCode(List<SubPayCur> payments) async{
+
+    List<String> data;
+    for (var element in payments) {
+      data.add(
+          {
+            "MAIN_TYPE": element.mainType,
+            "SUB_TYPE": element.subType,
+            "SUB_TYPE_DESC": element.subTypeDesc,
+            "YEAR_ID": element.yearId,
+            "CHK_DATE": element.chkDate,
+            "CHK_NO": element.chkNo,
+            "CHK_AMT": element.chkAmt,
+            "DUE": element.due,
+            "REASON_ID": element.reasonId,
+            "PAY_NO": element.payNo,
+            "SEQ": element.seq,
+            "CHECKED": element.isChecked ? "1" : ""
+          }.toString()
+      );
+    }
+    final response = await payRepository.issuanceOfUnifiedPaymentCodeService(data);
+    return response;
+  }
 
   void notifyMe() {
     notifyListeners();
