486499b5e0bb86deb6f34de269b5c0394a3e6696
wesamobeidat <wesam.obe@hotmail.com>
2022-12-11 14:20:43 +0300

    update language from profileScreen.dart

--- lib/src/view/profile/profileScreen.dart
+++ lib/src/view/profile/profileScreen.dart
@@ -2,10 +2,14 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:ssc/infrastructure/userConfig.dart';
+import 'package:ssc/src/viewModel/utilities/language/globalAppProvider.dart';
 import 'package:ssc/utilities/hexColor.dart';
 
 import '../../../infrastructure/userSecuredStorage.dart';
+import '../../../utilities/constants.dart';
 import '../../../utilities/util.dart';
 import 'dart:math' as math;
 
@@ -18,9 +22,25 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   UserSecuredStorage userSecuredStorage = UserSecuredStorage.instance;
+  Future<SharedPreferences> prefs = SharedPreferences.getInstance();
+
+  String selectedTheme;
+  String selectedLanguage;
+  String selectedTextSize;
+
+  getAppThemeAndLanguage(){
+    prefs.then((value) {
+      setState((){
+        selectedTheme = value.getString(Constants.APP_THEME) ?? Constants.SYSTEM_DEFAULT;
+        selectedLanguage = value.getString('language_code') ?? 'en';
+        selectedTextSize = value.getString('text_size') ?? 's';
+      });
+    });
+  }
 
   @override
   void initState(){
+    getAppThemeAndLanguage();
     // ProfileProvider profileProvider = Provider.of<ProfileProvider>(context, listen: false);
     super.initState();
   }
@@ -114,7 +134,16 @@ class _ProfileScreenState extends State<ProfileScreen> {
                     ),
                     const SizedBox(height: 10.0),
                     // buildButton('assets/icons/profileIcons/notifications.svg', 'notifications', (){}, withArrowIcon: false, hint: translate('disable', context)),
-                    buildButton('assets/icons/profileIcons/language.svg', 'language', (){}, withArrowIcon: false, hint: UserConfig.instance.language),
+                    buildButton('assets/icons/profileIcons/language.svg', 'language', (){
+                      setState(() {
+                        selectedLanguage = UserConfig.instance.checkLanguage() ? "ar" : "en";
+                      });
+                      Provider.of<GlobalAppProvider>(context, listen: false).changeLanguage(Locale(selectedLanguage));
+                      Provider.of<GlobalAppProvider>(context, listen: false).notifyMe();
+                      prefs.then((value) {
+                        value.setString('language_code', selectedLanguage);
+                      });
+                    }, withArrowIcon: false, hint: UserConfig.instance.language),
                     buildButton('assets/icons/profileIcons/security.svg', 'passwordAndSecurity', (){}),
                   ],
                 ),
