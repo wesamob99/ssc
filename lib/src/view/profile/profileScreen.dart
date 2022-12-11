77ef130c3bc3349108bcf440a6e2db8a27f8e99c
wesamobeidat <wesam.obe@hotmail.com>
2022-12-11 10:31:32 +0300

    update profileScreen.dart UI

--- lib/src/view/profile/profileScreen.dart
+++ lib/src/view/profile/profileScreen.dart
@@ -1,13 +1,12 @@
 // ignore_for_file: file_names
 
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:ssc/models/profile/userProfileData.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:ssc/infrastructure/userConfig.dart';
+import 'package:ssc/utilities/hexColor.dart';
 
 import '../../../infrastructure/userSecuredStorage.dart';
 import '../../../utilities/util.dart';
-import '../../viewModel/profile/profileProvider.dart';
 
 class ProfileScreen extends StatefulWidget {
   const ProfileScreen({Key key}) : super(key: key);
@@ -18,12 +17,10 @@ class ProfileScreen extends StatefulWidget {
 
 class _ProfileScreenState extends State<ProfileScreen> {
   UserSecuredStorage userSecuredStorage = UserSecuredStorage.instance;
-  Future accountDataFuture;
 
   @override
   void initState(){
-    ProfileProvider profileProvider = Provider.of<ProfileProvider>(context, listen: false);
-    accountDataFuture = profileProvider.getAccountData();
+    // ProfileProvider profileProvider = Provider.of<ProfileProvider>(context, listen: false);
     super.initState();
   }
 
@@ -32,83 +29,161 @@ class _ProfileScreenState extends State<ProfileScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: false,
-        title: Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children:[
-            Text(translate('hello', context), style: const TextStyle(fontSize: 14),),
-            Text(userSecuredStorage.userName, style: const TextStyle(fontSize: 14),),
-          ],
-        ),
+        title: Text(translate('accountSettings', context), style: const TextStyle(fontSize: 14),),
         leading: leadingBackIcon(context),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: FutureBuilder(
-              future: accountDataFuture,
-              builder: (context, snapshot){
-
-                switch(snapshot.connectionState){
-                  case ConnectionState.none:
-                    return somethingWrongWidget(context, 'somethingWrongHappened', 'somethingWrongHappenedDesc'); break;
-                  case ConnectionState.waiting:
-                  case ConnectionState.active:
-                    return Container(
-                        height: height(0.7, context),
-                        alignment: Alignment.center,
-                        child: animatedLoader(context)
-                    ); break;
-                  case ConnectionState.done:
-                    if(snapshot.hasData && !snapshot.hasError){
-                      UserProfileData userProfileData = snapshot.data;
-                      CurGetdatum data = userProfileData.curGetdata[0][0];
-                      if (kDebugMode) {
-                        print(data);
-                      }
-                      return SizedBox(
-                        height: height(1, context),
-                        width: width(1, context),
-                        child: Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                const Text("name"),
-                                Text(data.userName.toString())
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                const Text("email"),
-                                Text(data.email.toString())
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                const Text("gender"),
-                                Text(data.gender.toString())
-                              ],
-                            ),
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                const Text("mobile number"),
-                                Text(data.mobilenumber.toString())
-                              ],
-                            )
-                          ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.all(12.0),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(8.0)
+                ),
+                width: width(1, context),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          userSecuredStorage.userName,
+                          style: TextStyle(
+                            color: HexColor('#445740'),
+                          ),
                         ),
-                      );
-                    }
-                    break;
-                }
-                return somethingWrongWidget(context, 'somethingWrongHappened', 'somethingWrongHappenedDesc');
-              }
+                        SvgPicture.asset('assets/icons/profileIcons/edit.svg')
+                      ],
+                    ),
+                    const SizedBox(height: 10.0),
+                    Text(
+                      translate('modifyAndManageYourAccountDetails', context),
+                      style: TextStyle(
+                        color: HexColor('#999A9A'),
+                        fontSize: 12.0
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              Container(
+                padding: const EdgeInsets.all(12.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0)
+                ),
+                width: width(1, context),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      translate('myAccountSettings', context),
+                      style: TextStyle(
+                        color: HexColor('#445740')
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    buttonBuild('assets/icons/profileIcons/star.svg', 'invoicesAndPayments', (){}),
+                    buttonBuild('assets/icons/profileIcons/hand.svg', 'theSubscriptions', (){}),
+                    buttonBuild('assets/icons/profileIcons/dollar.svg', 'paymentMethods', (){}),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              Container(
+                padding: const EdgeInsets.all(12.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0)
+                ),
+                width: width(1, context),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      translate('preferences', context),
+                      style: TextStyle(
+                          color: HexColor('#445740')
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    buttonBuild('assets/icons/profileIcons/notifications.svg', 'notifications', (){}, withArrowIcon: false, hint: translate('disable', context)),
+                    buttonBuild('assets/icons/profileIcons/language.svg', 'language', (){}, withArrowIcon: false, hint: UserConfig.instance.language),
+                    buttonBuild('assets/icons/profileIcons/security.svg', 'passwordAndSecurity', (){}),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10.0),
+              Container(
+                padding: const EdgeInsets.all(12.0),
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(8.0)
+                ),
+                width: width(1, context),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      translate('aboutUs', context),
+                      style: TextStyle(
+                          color: HexColor('#445740')
+                      ),
+                    ),
+                    const SizedBox(height: 10.0),
+                    buttonBuild('assets/icons/profileIcons/headphone.svg', 'aboutUs', (){}),
+                    buttonBuild('assets/icons/profileIcons/list.svg', 'termsAndConditions', (){}),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
       ),
     );
   }
+
+  buttonBuild(String icon, String text, void Function() onTap, {bool withArrowIcon = true, String hint = ''}){
+    print(hint);
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5.0),
+      child: InkWell(
+        onTap: onTap,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Row(
+              children: [
+                SvgPicture.asset(icon),
+                const SizedBox(width: 10.0),
+                Text(
+                  translate(text, context),
+                  style: TextStyle(
+                      color: HexColor('#445740')
+                  ),
+                ),
+                SizedBox(width: hint != '' ? 12.0 : 0),
+                hint != ''
+                ? Text(
+                  hint,
+                  style: TextStyle(
+                    color: HexColor('#999A9A'),
+                    fontSize: 12.0
+                  ),
+                ) : const SizedBox.shrink(),
+              ],
+            ),
+            if(withArrowIcon)
+            SvgPicture.asset('assets/icons/profileIcons/arrow.svg'),
+          ],
+        ),
+      ),
+    );
+  }
+
 }
