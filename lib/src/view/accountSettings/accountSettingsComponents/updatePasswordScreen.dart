f63b83c197fad6d04abae481ee161774a49200cb
wesamobeidat <wesam.obe@hotmail.com>
2022-12-15 14:52:12 +0300

    create updatePasswordScreen.dart

---
+++ lib/src/view/accountSettings/accountSettingsComponents/updatePasswordScreen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UpdatePasswordScreen extends StatefulWidget {
+  const UpdatePasswordScreen({Key key}) : super(key: key);
+
+  @override
+  State<UpdatePasswordScreen> createState() => _UpdatePasswordScreenState();
+}
+
+class _UpdatePasswordScreenState extends State<UpdatePasswordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
