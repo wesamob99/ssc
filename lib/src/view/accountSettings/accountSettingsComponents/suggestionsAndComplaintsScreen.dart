2539ecdf152f29519f9fe5820106d3b8348717fe
wesamobeidat <wesam.obe@hotmail.com>
2022-12-18 09:13:35 +0300

    create suggestionsAndComplaintsScreen.dart

---
+++ lib/src/view/accountSettings/accountSettingsComponents/suggestionsAndComplaintsScreen.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class SuggestionsAndComplaintsScreen extends StatefulWidget {
+  const SuggestionsAndComplaintsScreen({Key key}) : super(key: key);
+
+  @override
+  State<SuggestionsAndComplaintsScreen> createState() => _SuggestionsAndComplaintsScreenState();
+}
+
+class _SuggestionsAndComplaintsScreenState extends State<SuggestionsAndComplaintsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
