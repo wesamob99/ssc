9e94e84e242e7538600199ee4ac49833c992b53d
wesamobeidat <wesam.obe@hotmail.com>
2022-12-27 11:20:50 +0300

    create historicalPensionDetailsScreen.dart

---
+++ lib/src/view/services/retirementServices/screens/historicalPensionDetailsScreen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HistoricalPensionDetailsScreen extends StatefulWidget {
+  const HistoricalPensionDetailsScreen({Key key}) : super(key: key);
+
+  @override
+  State<HistoricalPensionDetailsScreen> createState() => _HistoricalPensionDetailsScreenState();
+}
+
+class _HistoricalPensionDetailsScreenState extends State<HistoricalPensionDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
