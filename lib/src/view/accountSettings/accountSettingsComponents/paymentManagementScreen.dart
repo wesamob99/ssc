9a4a385d23a9593200ffccf7a5caf8c8b5331f8e
wesamobeidat <wesam.obe@hotmail.com>
2022-12-29 10:51:19 +0300

    finish paymentManagementScreen.dart UI

--- lib/src/view/accountSettings/accountSettingsComponents/paymentManagementScreen.dart
+++ lib/src/view/accountSettings/accountSettingsComponents/paymentManagementScreen.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: file_names
 
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:provider/provider.dart';
@@ -10,6 +11,7 @@ import 'package:ssc/src/viewModel/utilities/theme/themeProvider.dart';
 import '../../../../utilities/hexColor.dart';
 import '../../../../utilities/theme/themes.dart';
 import '../../../../utilities/util.dart';
+import 'dart:ui' as ui;
 
 class PaymentManagementScreen extends StatefulWidget {
   const PaymentManagementScreen({Key key}) : super(key: key);
@@ -221,80 +223,232 @@ class _PaymentManagementScreenState extends State<PaymentManagementScreen> {
     );
   }
 
-
   buildPaymentCard(String title, String amount, int status, String cardNo){
     String statusText = '';
     status == 1 ? statusText = 'waiting' : statusText = 'paymentCompleted';
-    return Padding(
-      padding: const EdgeInsets.only(bottom: 20.0),
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                UserConfig.instance.checkLanguage()
-                    ? 'Pay an optional subscription fee' : 'تسديد رسوم اشتراك اختياري',
-                style: TextStyle(
-                  color: HexColor('#363636'),
-                  fontSize: 14,
-                ),
-              ),
-              const SizedBox(height: 10.0,),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 5.0),
-                decoration: BoxDecoration(
-                    color: status == 1
-                      ? const Color.fromRGBO(221, 201, 129, 0.49)
-                      : const Color.fromRGBO(129, 221, 199, 0.49),
-                    borderRadius: BorderRadius.circular(50.0)
-                ),
-                child: Text(
-                  translate(statusText, context),
+    return InkWell(
+      onTap: () => paymentDetailsModalBottomSheet(),
+      child: Padding(
+        padding: const EdgeInsets.only(bottom: 10.0, top: 10),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  UserConfig.instance.checkLanguage()
+                      ? 'Pay an optional subscription fee' : 'تسديد رسوم اشتراك اختياري',
                   style: TextStyle(
-                    color: status == 1 ? HexColor('#987803') : HexColor('#248389'),
+                    color: HexColor('#363636'),
                     fontSize: 14,
                   ),
                 ),
-              ),
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: [
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Row(
-                    children: [
-                      Text(
-                        amount,
-                        style: TextStyle(
-                          color: HexColor('#363636'),
-                          fontWeight: FontWeight.bold,
-                          fontSize: 19,
-                        ),
-                      ),
-                      Text(
-                        ' ${translate('jd', context)}',
-                        style: TextStyle(
-                          color: HexColor('#363636'),
-                          fontWeight: FontWeight.w400,
-                          fontSize: 13,
-                        ),
-                      ),
-                    ],
+                const SizedBox(height: 10.0,),
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 5.0),
+                  decoration: BoxDecoration(
+                      color: status == 1
+                        ? const Color.fromRGBO(221, 201, 129, 0.49)
+                        : const Color.fromRGBO(129, 221, 199, 0.49),
+                      borderRadius: BorderRadius.circular(50.0)
                   ),
-                  const SizedBox(height: 10.0,),
-                  Container(
-                    padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 5.0),
-                    decoration: BoxDecoration(
-                        color: const Color.fromRGBO(220, 220, 220, 0.49),
-                        borderRadius: BorderRadius.circular(50.0)
+                  child: Text(
+                    translate(statusText, context),
+                    style: TextStyle(
+                      color: status == 1 ? HexColor('#987803') : HexColor('#248389'),
+                      fontSize: 14,
                     ),
-                    child: Row(
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Text(
+                          amount,
+                          style: TextStyle(
+                            color: HexColor('#363636'),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 19,
+                          ),
+                        ),
+                        Text(
+                          ' ${translate('jd', context)}',
+                          style: TextStyle(
+                            color: HexColor('#363636'),
+                            fontWeight: FontWeight.w400,
+                            fontSize: 13,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 10.0,),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 5.0),
+                      decoration: BoxDecoration(
+                          color: const Color.fromRGBO(220, 220, 220, 0.49),
+                          borderRadius: BorderRadius.circular(50.0)
+                      ),
+                      child: Row(
+                        children: [
+                          Text(
+                            translate('card', context),
+                            style: TextStyle(
+                              color: HexColor('#716F6F'),
+                              fontSize: 14,
+                            ),
+                          ),
+                          Text(
+                            ' $cardNo',
+                            style: TextStyle(
+                              color: HexColor('#716F6F'),
+                              fontWeight: FontWeight.bold,
+                              fontSize: 14,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(width: 25.0,),
+                SvgPicture.asset('assets/icons/arrow.svg')
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  paymentDetailsModalBottomSheet(){
+    return showModalBottomSheet(
+        shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(25.0))
+        ),
+        context: context,
+        barrierColor: Colors.black26,
+        isScrollControlled: true,
+        constraints: BoxConstraints(
+          maxHeight: height(0.9, context)
+        ),
+        builder: (context) {
+          return BackdropFilter(
+            filter: ui.ImageFilter.blur(
+              sigmaX: 1.0,
+              sigmaY: 1.0,
+            ),
+            child: Material(
+              elevation: 100,
+              borderRadius: BorderRadius.circular(25.0),
+              color: Colors.white,
+              shadowColor: Colors.black,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 10.0).copyWith(top: 15.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          width: 45,
+                          height: 6,
+                          decoration: BoxDecoration(
+                              color: HexColor('#000000'),
+                              borderRadius: const BorderRadius.all(Radius.circular(25.0))),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 25.0,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          translate('paymentDetail', context),
+                          style: TextStyle(
+                            color: HexColor('#363636'),
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                          ),
+                        ),
+                        InkWell(
+                          onTap: (){
+                            Navigator.of(context).pop();
+                          },
+                          child: SvgPicture.asset('assets/icons/close.svg'),
+                        )
+                      ],
+                    ),
+                    const SizedBox(height: 25.0,),
+                    SizedBox(
+                      width: width(1, context),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            UserConfig.instance.checkLanguage()
+                                ? 'Pay an optional subscription fee' : 'تسديد رسوم اشتراك اختياري',
+                            style: TextStyle(
+                              color: HexColor('#363636'),
+                              fontSize: 16,
+                            ),
+                          ),
+                          const SizedBox(height: 15.0,),
+                          Text(
+                            '25/12/2022',
+                            style: TextStyle(
+                              color: HexColor('#363636'),
+                              fontWeight: FontWeight.w600,
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 30.0,),
+                    Text(
+                      UserConfig.instance.checkLanguage()
+                          ? 'The amount paid' : 'المبلغ المدفوع',
+                      style: TextStyle(
+                        color: HexColor('#363636'),
+                        fontSize: 16,
+                      ),
+                    ),
+                    const SizedBox(height: 15.0,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          '250',
+                          style: TextStyle(
+                            color: HexColor('#363636'),
+                            fontWeight: FontWeight.w700,
+                            fontSize: 35,
+                          ),
+                        ),
+                        Text(
+                          ' ${translate('jd', context)}',
+                          style: TextStyle(
+                            color: HexColor('#363636'),
+                            fontWeight: FontWeight.bold,
+                            fontSize: 15,
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 15.0,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
                       children: [
                         Text(
                           translate('card', context),
@@ -304,7 +458,7 @@ class _PaymentManagementScreenState extends State<PaymentManagementScreen> {
                           ),
                         ),
                         Text(
-                          ' $cardNo',
+                          ' 5656',
                           style: TextStyle(
                             color: HexColor('#716F6F'),
                             fontWeight: FontWeight.bold,
@@ -313,15 +467,120 @@ class _PaymentManagementScreenState extends State<PaymentManagementScreen> {
                         ),
                       ],
                     ),
-                  ),
-                ],
+                    Padding(
+                      padding: const EdgeInsets.only(top: 40.0, bottom: 30.0),
+                      child: DottedBorder(
+                        radius: const Radius.circular(8.0),
+                        padding: EdgeInsets.zero,
+                        color: HexColor('#979797'),
+                        borderType: BorderType.Rect,
+                        dashPattern: const [8],
+                        strokeWidth: 1.2, child: Container(),
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: width(0.1, context)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                translate('paymentStatus', context),
+                                style: TextStyle(
+                                  color: HexColor('#979797'),
+                                  fontSize: 14,
+                                ),
+                              ),
+                              Text(
+                                translate('paymentDueDate', context),
+                                style: TextStyle(
+                                  color: HexColor('#979797'),
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 15.0,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                translate('payed', context),
+                                style: TextStyle(
+                                  color: HexColor('#595959'),
+                                  fontSize: 14,
+                                ),
+                              ),
+                              Text(
+                                '25/12/2022',
+                                style: TextStyle(
+                                  color: HexColor('#595959'),
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 40,),
+                    Padding(
+                      padding: EdgeInsets.symmetric(horizontal: width(0.1, context)),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                translate('invoiceNumber', context),
+                                style: TextStyle(
+                                  color: HexColor('#979797'),
+                                  fontSize: 14,
+                                ),
+                              ),
+                              Text(
+                                translate('payDate', context),
+                                style: TextStyle(
+                                  color: HexColor('#979797'),
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                          const SizedBox(height: 15.0,),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(
+                                '435975',
+                                style: TextStyle(
+                                  color: HexColor('#595959'),
+                                  fontSize: 14,
+                                ),
+                              ),
+                              Text(
+                                '25/12/2022',
+                                style: TextStyle(
+                                  color: HexColor('#595959'),
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
               ),
-              const SizedBox(width: 25.0,),
-              SvgPicture.asset('assets/icons/arrow.svg')
-            ],
-          )
-        ],
-      ),
+            ),
+          );
+        }
     );
   }
 }
