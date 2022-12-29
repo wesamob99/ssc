870f5659cad6f1f91f2c0aca57ae171100bc8b41
wesamobeidat <wesam.obe@hotmail.com>
2022-12-29 11:24:26 +0300

    create accountStatementScreen.dart

---
+++ lib/src/view/accountSettings/accountSettingsComponents/accountStatementScreen.dart
@@ -0,0 +1,17 @@
+// ignore_for_file: file_names
+
+import 'package:flutter/material.dart';
+
+class AccountStatementScreen extends StatefulWidget {
+  const AccountStatementScreen({Key key}) : super(key: key);
+
+  @override
+  State<AccountStatementScreen> createState() => _AccountStatementScreenState();
+}
+
+class _AccountStatementScreenState extends State<AccountStatementScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
