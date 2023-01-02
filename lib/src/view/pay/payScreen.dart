2ecb9aa6db6aae1531062e9daad2dd9d45a303b2
wesamobeidat <wesam.obe@hotmail.com>
2023-01-02 10:21:13 +0300

    payScreen.dart update (dark mode)

--- lib/src/view/pay/payScreen.dart
+++ lib/src/view/pay/payScreen.dart
@@ -345,7 +345,7 @@ class _PayScreenState extends State<PayScreen> {
             duration: const Duration(milliseconds: 300),
             width: width(1, context),
             height: height(1, context),
-            color: Colors.white70,
+            color: themeNotifier.isLight() ? Colors.white70 : Colors.black45,
             child: Center(
               child: animatedLoader(context),
             ),
