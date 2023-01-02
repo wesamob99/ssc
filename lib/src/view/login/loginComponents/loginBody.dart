c36959bb8fa7ea9067ead7f430f07381c0ce53eb
wesamobeidat <wesam.obe@hotmail.com>
2023-01-02 10:20:50 +0300

    loginBody.dart update (dark mode)

--- lib/src/view/login/loginComponents/loginBody.dart
+++ lib/src/view/login/loginComponents/loginBody.dart
@@ -244,7 +244,7 @@ class _LoginBodyState extends State<LoginBody> {
           duration: const Duration(milliseconds: 300),
           width: width(1, context),
           height: height(1, context),
-          color: Colors.white70,
+          color: themeNotifier.isLight() ? Colors.white70 : Colors.black45,
           child: Center(
             child: animatedLoader(context),
           ),
