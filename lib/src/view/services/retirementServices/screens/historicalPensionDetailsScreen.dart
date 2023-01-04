4718408472d9f07fe6200a4452ea68b41ce2077a
wesamobeidat <wesam.obe@hotmail.com>
2023-01-04 15:07:33 +0300

    update historicalPensionDetailsScreen.dart card UI finished

--- lib/src/view/services/retirementServices/screens/historicalPensionDetailsScreen.dart
+++ lib/src/view/services/retirementServices/screens/historicalPensionDetailsScreen.dart
@@ -7,9 +7,11 @@ import 'package:intl/intl.dart';
 import 'package:provider/provider.dart';
 import 'package:ssc/models/services/pensionPaymentModel.dart';
 import 'package:ssc/src/viewModel/services/servicesProvider.dart';
+import 'package:ssc/src/viewModel/utilities/theme/themeProvider.dart';
 
 import '../../../../../infrastructure/userConfig.dart';
 import '../../../../../utilities/hexColor.dart';
+import '../../../../../utilities/theme/themes.dart';
 import '../../../../../utilities/util.dart';
 
 class HistoricalPensionDetailsScreen extends StatefulWidget {
@@ -22,12 +24,14 @@ class HistoricalPensionDetailsScreen extends StatefulWidget {
 class _HistoricalPensionDetailsScreenState extends State<HistoricalPensionDetailsScreen> {
 
   ServicesProvider servicesProvider;
+  ThemeNotifier themeNotifier;
   List<SelectedListItem> listOfYears = [];
   SelectedListItem selectedYear;
 
   @override
   void initState() {
     servicesProvider = Provider.of<ServicesProvider>(context, listen: false);
+    themeNotifier = Provider.of<ThemeNotifier>(context, listen: false);
     listOfYears = [];
     selectedYear = SelectedListItem(name: DateFormat("dd/MM/yyyy").parse(servicesProvider.result['cur_getdata'][0][0]['STARDT']).year.toString(), natCode: null, flag: '');
     for(int i=DateFormat("dd/MM/yyyy").parse(servicesProvider.result['cur_getdata'][0][0]['STARDT']).year ; i<=2022 ; i++){
@@ -69,30 +73,99 @@ class _HistoricalPensionDetailsScreenState extends State<HistoricalPensionDetail
                       if(snapshot.hasData && !snapshot.hasError){
                         PensionPaymentModel pensionPayment = snapshot.data;
                         if(pensionPayment.curGetdata1.isNotEmpty){
-                          return Stack(
-                            children: [
-                              SizedBox(
-                                height: height(0.5, context),
-                                child: ListView.builder(
+                          return Expanded(
+                            child: Stack(
+                              children: [
+                                ListView.builder(
                                   itemCount: pensionPayment.curGetdata1[0].length,
                                   itemBuilder: (context, index){
-                                    return Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      children: [
-                                        Text(pensionPayment.curGetdata1[0][index].netpay1),
-                                        const SizedBox(height: 10.0,),
-                                      ],
+                                    return Card(
+                                        elevation: 6.0,
+                                        shadowColor: Colors.black45,
+                                        color: getContainerColor(context),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(15.0),
+                                        ),
+                                        child: Container(
+                                          width: width(1, context),
+                                          padding: const EdgeInsets.all(20.0),
+                                          child: Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            children: [
+                                              Text(
+                                                pensionPayment.curGetdata1[0][index].cod.toString(),
+                                                style: TextStyle(
+                                                  height: 1.4,
+                                                  color: themeNotifier.isLight() ? HexColor('#363636') : Colors.white,
+                                                  fontWeight: FontWeight.bold,
+                                                ),
+                                              ),
+                                              const SizedBox(height: 15.0,),
+                                              Text(
+                                                pensionPayment.curGetdata1[0][index].mth.toString(),
+                                                style: TextStyle(
+                                                  color: themeNotifier.isLight() ? HexColor('#716F6F') : Colors.white70,
+                                                ),
+                                              ),
+                                              const SizedBox(height: 15.0,),
+                                              Divider(
+                                                color: HexColor('#A6A6A6'),
+                                                thickness: 1,
+                                              ),
+                                              const SizedBox(height: 10.0,),
+                                              Row(
+                                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                                crossAxisAlignment: CrossAxisAlignment.center,
+                                                children: [
+                                                  Text(
+                                                    translate('theAmount', context),
+                                                    style: TextStyle(
+                                                      color: themeNotifier.isLight() ? HexColor('#716F6F') : Colors.white,
+                                                    ),
+                                                  ),
+                                                  Row(
+                                                    children: [
+                                                      Text(
+                                                        pensionPayment.curGetdata1[0][index].netpay1.toString(),
+                                                        style: TextStyle(
+                                                          color: themeNotifier.isLight() ? HexColor('#716F6F') : Colors.white,
+                                                          fontWeight: FontWeight.bold,
+                                                          fontSize: 19,
+                                                        ),
+                                                      ),
+                                                      Text(
+                                                        ' ${translate('jd', context)}',
+                                                        style: TextStyle(
+                                                          color: themeNotifier.isLight() ? HexColor('#716F6F') : Colors.white,
+                                                          fontWeight: FontWeight.w400,
+                                                          fontSize: 13,
+                                                        ),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ],
+                                              ),
+                                            ],
+                                          ),
+                                        )
                                     );
+                                    // return Column(
+                                    //   crossAxisAlignment: CrossAxisAlignment.start,
+                                    //   children: [
+                                    //     Text(pensionPayment.curGetdata1[0][index].netpay1),
+                                    //     const SizedBox(height: 10.0,),
+                                    //   ],
+                                    // );
                                   },
                                 ),
-                              ),
-                              if(Provider.of<ServicesProvider>(context).isLoading)
-                                Expanded(
-                                  child: Center(
-                                    child: animatedLoader(context),
+                                if(Provider.of<ServicesProvider>(context).isLoading)
+                                  Expanded(
+                                    child: Center(
+                                      child: animatedLoader(context),
+                                    ),
                                   ),
-                                ),
-                            ],
+                              ],
+                            ),
                           );
                         }else {
                           return Text(translate('thereAreNoData', context));
