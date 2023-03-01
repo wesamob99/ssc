884cc8b8997a04a872ce9616eeb3cda848f3b161
wesamobeidat <wesam.obe@hotmail.com>
2023-03-01 11:03:52 +0300

    update aboutTheServiceScreen.dart

--- lib/source/view/services/shared/aboutTheServiceScreen.dart
+++ lib/source/view/services/shared/aboutTheServiceScreen.dart
@@ -285,7 +285,8 @@ class _AboutTheServiceScreenState extends State<AboutTheServiceScreen> {
                                           ((widget.serviceTitle == 'requestToAmendTheAnnualIncreasePercentage') && value["PO_status_no"] == null) || /// request to amend the annual increase percentage - طلب تعديل نسبة الزيادة السنوية
                                           ((widget.serviceTitle == 'historicalPensionDetails') && value['cur_getdata'].length != 0) || /// historical pension details - الرواتب التقاعديه التاريخيه
                                           ((widget.serviceTitle == 'earlyRetirementRequest') && value['P_Message'][0][0]['PO_STATUS'] == 0) || /// early retirement - تقاعد مبكر
-                                          ((widget.serviceTitle == 'report_a_sickness/work_injury_complaint') && value != null) /// report_a_sickness/work_injury_complaint - تبليغ عن حادث العمل / مرض مهني
+                                          ((widget.serviceTitle == 'report_a_sickness/work_injury_complaint') && value != null) || /// report_a_sickness/work_injury_complaint - تبليغ عن حادث العمل / مرض مهني
+                                          ((widget.serviceTitle == 'deceasedRetirementApplication') && value != null) /// report_a_sickness/work_injury_complaint - تبليغ عن حادث العمل / مرض مهني
                                         )
                                       ){
                                         servicesProvider.result = value;
