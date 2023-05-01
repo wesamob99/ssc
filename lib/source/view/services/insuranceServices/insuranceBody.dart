b63cb36ace667f14ee810cbca54cd37d0323a83f
wesamobeidat <wesam.obe@hotmail.com>
2023-05-01 15:13:08 +0300

    update

--- lib/source/view/services/insuranceServices/insuranceBody.dart
+++ lib/source/view/services/insuranceServices/insuranceBody.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:ssc/source/view/services/insuranceServices/screens/oneTimeRefundInquiry.dart';
 import 'package:ssc/source/view/services/shared/aboutTheServiceScreen.dart';
 import 'package:ssc/source/view/services/shared/servicesListConstants.dart';
 
@@ -69,27 +70,51 @@ class _InsuranceBodyState extends State<InsuranceBody> {
                       children: [
                         InkWell(
                           onTap: (){
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                  builder: (context) => AboutTheServiceScreen(
-                                    serviceScreen: insuranceBenefitsServices[index2].screen,
-                                    serviceTitle: insuranceBenefitsServices[index2].title,
-                                    aboutServiceDescription: insuranceBenefitsServices[index2].description,
-                                    termsOfTheService: const [
-                                      'موظفي القطاع الخاص',
-                                      'موظف موقوف عن العمل',
-                                      'لديك 36 اشتراك او رصيد اكثر من 300 د.ا',
-                                      'ان تكون قد استفدت من بدل التعطل ثلاث مرات او اقل خلال فتره الشمول',
-                                    ],
-                                    stepsOfTheService: const [
-                                      'التأكد من المعلومات الشخصية لمقدم الخدمة',
-                                      'تعبئة طلب الخدمة',
-                                      'تقديم الطلب'
-                                    ],
-                                    serviceApiCall: insuranceBenefitsServices[index2].serviceApiCall
-                                  )
-                              ),
-                            );
+                            if(insuranceBenefitsServices[index2].title == 'oneTimeCompensationRequest'){
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                    builder: (context) => OneTimeRefundInquiryScreen(
+                                        serviceScreen: insuranceBenefitsServices[index2].screen,
+                                        serviceTitle: insuranceBenefitsServices[index2].title,
+                                        aboutServiceDescription: insuranceBenefitsServices[index2].description,
+                                        termsOfTheService: const [
+                                          'موظفي القطاع الخاص',
+                                          'موظف موقوف عن العمل',
+                                          'لديك 36 اشتراك او رصيد اكثر من 300 د.ا',
+                                          'ان تكون قد استفدت من بدل التعطل ثلاث مرات او اقل خلال فتره الشمول',
+                                        ],
+                                        stepsOfTheService: const [
+                                          'التأكد من المعلومات الشخصية لمقدم الخدمة',
+                                          'تعبئة طلب الخدمة',
+                                          'تقديم الطلب'
+                                        ],
+                                        serviceApiCall: insuranceBenefitsServices[index2].serviceApiCall
+                                    )
+                                ),
+                              );
+                            }else{
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                    builder: (context) => AboutTheServiceScreen(
+                                        serviceScreen: insuranceBenefitsServices[index2].screen,
+                                        serviceTitle: insuranceBenefitsServices[index2].title,
+                                        aboutServiceDescription: insuranceBenefitsServices[index2].description,
+                                        termsOfTheService: const [
+                                          'موظفي القطاع الخاص',
+                                          'موظف موقوف عن العمل',
+                                          'لديك 36 اشتراك او رصيد اكثر من 300 د.ا',
+                                          'ان تكون قد استفدت من بدل التعطل ثلاث مرات او اقل خلال فتره الشمول',
+                                        ],
+                                        stepsOfTheService: const [
+                                          'التأكد من المعلومات الشخصية لمقدم الخدمة',
+                                          'تعبئة طلب الخدمة',
+                                          'تقديم الطلب'
+                                        ],
+                                        serviceApiCall: insuranceBenefitsServices[index2].serviceApiCall
+                                    )
+                                ),
+                              );
+                            }
                           },
                           child: Container(
                             margin: EdgeInsets.only(left: 15.0, right: 15.0, top: 10.0, bottom: (last != -1 && last != index2) ? 15.0 : 25.0),
