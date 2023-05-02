79ff10d9f60d9a839029925154077926518d6677
wesamobeidat <wesam.obe@hotmail.com>
2023-05-02 12:45:42 +0300

    update

--- lib/source/view/services/shared/paymentScreen.dart
+++ lib/source/view/services/shared/paymentScreen.dart
@@ -3,8 +3,10 @@
 import 'package:drop_down_list/drop_down_list.dart';
 import 'package:drop_down_list/model/selected_list_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 import 'package:provider/provider.dart';
 import 'package:ssc/infrastructure/userConfig.dart';
+import 'package:ssc/utilities/theme/themes.dart';
 
 import '../../../../../utilities/hexColor.dart';
 import '../../../../../utilities/util.dart';
@@ -101,11 +103,84 @@ class _PaymentScreenState extends State<PaymentScreen> {
             SizedBox(height: height(0.02, context),),
             buildFieldTitle(context, 'methodOfReceivingTheAllowance', required: false),
             const SizedBox(height: 10.0,),
-            // customTwoRadioButtons(5, 'insideJordan', 'outsideJordan', setState),
-            SizedBox(
-              height: servicesProvider.activePayment.length * 40.0,
-              child: customRadioButtonGroup(servicesProvider.activePayment, setState),
+            if(servicesProvider.activePayment.contains(servicesProvider.activePayment.where((element) => [1, 3, 4].contains(element['ID'])).first) &&
+                servicesProvider.activePayment.contains(servicesProvider.activePayment.where((element) => element['ID']  == 5).first))
+            customTwoRadioButtons('insideJordan', 'outsideJordan', setState),
+            if(!servicesProvider.activePayment.contains(servicesProvider.activePayment.where((element) => [1, 3, 4].contains(element['ID'])).first) &&
+                servicesProvider.activePayment.contains(servicesProvider.activePayment.where((element) => element['ID']  == 5).first))
+            buildFieldTitle(context, 'outsideJordan', required: false),
+            if(servicesProvider.activePayment.contains(servicesProvider.activePayment.where((element) => [1, 3, 4].contains(element['ID'])).first) &&
+                !servicesProvider.activePayment.contains(servicesProvider.activePayment.where((element) => element['ID']  == 5).first))
+            buildFieldTitle(context, 'insideJordan', required: false),
+            const SizedBox(height: 10.0,),
+            if(servicesProvider.selectedInsideOutsideJordan == 'insideJordan')
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10.0),
+              child: SizedBox(
+                height: 110,
+                child: ListView.builder(
+                  itemCount: servicesProvider.activePayment.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index){
+                    return servicesProvider.activePayment[index]['ID'] != 5
+                    ? Row(
+                      children: [
+                        InkWell(
+                          onTap: (){
+                            setState(() {
+                              servicesProvider.selectedActivePayment = servicesProvider.activePayment[index];
+                              servicesProvider.notifyMe();
+                            });
+                          },
+                          child: Container(
+                            width: 100,
+                            height: 100,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(8.0),
+                              border: Border.all(
+                                color: (servicesProvider.selectedActivePayment == servicesProvider.activePayment[index]) ? primaryColor : HexColor('#C9C9C9'),
+                              ),
+                            ),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                SvgPicture.asset(
+                                  servicesProvider.activePayment[index]["ID"] == 3
+                                  ? 'assets/icons/bank.svg' : servicesProvider.activePayment[index]["ID"] == 1
+                                  ? 'assets/icons/moneycheck.svg' : 'assets/icons/eWallet.svg',
+                                  color: (servicesProvider.selectedActivePayment == servicesProvider.activePayment[index]) ? primaryColor : HexColor('#C9C9C9'),
+                                ),
+                                const SizedBox(height: 5.0,),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(horizontal: 2.0),
+                                  child: Text(
+                                    UserConfig.instance.isLanguageEnglish()
+                                    ? servicesProvider.activePayment[index]["NAME_EN"]
+                                    : servicesProvider.activePayment[index]["NAME_AR"],
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      color: (servicesProvider.selectedActivePayment == servicesProvider.activePayment[index]) ? primaryColor : HexColor('#C9C9C9'),
+                                      fontSize: 8.0,
+                                    ),
+                                  ),
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                        const SizedBox(width: 10.0,)
+                      ],
+                    ) : const SizedBox.shrink();
+                  },
+                ),
+              ),
             ),
+            if(servicesProvider.selectedInsideOutsideJordan == 'insideJordan')
+            Container(),
+            // SizedBox(
+            //   height: servicesProvider.activePayment.length * 40.0,
+            //   child: customRadioButtonGroup(servicesProvider.activePayment, setState),
+            // ),
             if(servicesProvider.selectedActivePayment['ID'] == 3) // inside jordan
               Text(
                 servicesProvider.result["p_per_info"][0][0]["IBAN"] != null && servicesProvider.result["p_per_info"][0][0]["IBAN"].length == 30
@@ -312,6 +387,99 @@ class _PaymentScreenState extends State<PaymentScreen> {
       ),
     );
   }
+
+  Widget customTwoRadioButtons(String firstChoice, String secondChoice, setState){
+    return Row(
+      children: [
+        InkWell(
+          onTap: (){
+            setState(() {
+              servicesProvider.selectedInsideOutsideJordan = firstChoice;
+              for (var element in servicesProvider.activePayment) {
+                if(element['ID'] == 1 || element['ID'] == 4 || element['ID'] == 3){
+                  if(servicesProvider.activePayment.contains(servicesProvider.activePayment.where((element) => element['ID']  == 3).first)){
+                    servicesProvider.selectedActivePayment = servicesProvider.activePayment.where((element) => element['ID']  == 3).first;
+                  } else{
+                    servicesProvider.selectedActivePayment = element;
+                  }
+                }
+              }
+              servicesProvider.notifyMe();
+            });
+          },
+          child: Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(500.0),
+                  border: Border.all(
+                    color: HexColor('#2D452E'),
+                  ),
+                ),
+                padding: const EdgeInsets.all(2.0),
+                child: CircleAvatar(
+                  radius: isTablet(context) ? 10 : 5,
+                  backgroundColor: (servicesProvider.selectedInsideOutsideJordan == firstChoice)
+                      ? HexColor('#2D452E')
+                      : Colors.transparent,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  getTranslated(firstChoice, context),
+                  style: TextStyle(
+                    color: HexColor('#666666'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        const SizedBox(width: 10.0,),
+        InkWell(
+          onTap: (){
+            setState(() {
+              servicesProvider.selectedInsideOutsideJordan = secondChoice;
+              servicesProvider.selectedActivePayment = servicesProvider.activePayment.where((element) => element['ID']  == 5).first;
+              servicesProvider.notifyMe();
+            });
+          },
+          child: Row(
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                  color: Colors.transparent,
+                  borderRadius: BorderRadius.circular(500.0),
+                  border: Border.all(
+                    color: HexColor('#2D452E'),
+                  ),
+                ),
+                padding: const EdgeInsets.all(2.0),
+                child: CircleAvatar(
+                  radius: isTablet(context) ? 10 : 5,
+                  backgroundColor: (servicesProvider.selectedInsideOutsideJordan == secondChoice)
+                      ? HexColor('#2D452E')
+                      : Colors.transparent,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text(
+                  getTranslated(secondChoice, context),
+                  style: TextStyle(
+                    color: HexColor('#666666'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+
 
 }
 
